d15653d222b3c369de276ec4d7c7875ec4ec63d4
Scott Cole <scott.cole0@gmail.com>
2017-08-28 17:02:24 -0700

    remove dist files

--- dist/neurodsp-0.1/neurodsp/shape/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cyclefeatures import features_by_cycle
-from .cyclepoints import find_extrema, find_zerox
-from .phase import extrema_interpolated_phase
-from .swm import sliding_window_matching
