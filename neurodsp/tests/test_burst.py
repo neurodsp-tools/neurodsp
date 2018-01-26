0b757a18223bbde118ae46fbbf5c360a69229f4f
Andrew Washington <awashington07@gmail.com>
2018-01-25 22:22:17 -0800

    Introduced consistency test for detect_bursts()

---
+++ neurodsp/tests/test_burst.py
@@ -0,0 +1,38 @@
+"""
+test_burst.py
+Test burst detection functions
+"""
+
+import os
+import pytest
+import numpy as np
+import neurodsp
+from neurodsp.tests import _load_example_data
+ 
+
+def test_detect_bursts_consistent():
+    """
+    Confirm consistency in burst detection results on a generated neural signal
+    """
+    # Load data and ground-truth filtered signal
+    x = _load_example_data(data_idx=1)
+    bursting_true = np.load(os.path.dirname(neurodsp.__file__) +
+                            '/tests/data/sample_data_1_burst_deviation.npy')
+
+    # Detect bursts
+    Fs = 1000
+    f_lo = 8
+    f_hi = 12
+    
+    bursting = neurodsp.detect_bursts(Fs, x, (f_lo, f_hi),
+                                      algorithm='deviation', thresh=(0.9, 2.0))
+    assert np.isclose(np.sum(bursting-bursting_true), 0)
+
+def test_detect_bursts_length():
+    """
+    Confirm length of burst detection results on a generated neural signal
+    """
+    raise NotImplementedError
+
+    bursting = neurodsp.detect_bursts(Fs, x, alpha, algorithm='deviation', thresh=(3, 3))
+
