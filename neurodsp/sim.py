4e912eae0dfc0e239259450ffb2cd07388c9ca71
Scott Cole <scott.cole0@gmail.com>
2018-03-27 16:34:15 -0700

    autopep8

--- neurodsp/sim.py
+++ neurodsp/sim.py
@@ -10,7 +10,7 @@ from scipy import signal
 
 def sim_filtered_brown_noise(T, Fs, f_range, N):
     """Simulate a band-pass filtered signal with brown noise
-    
+
     Parameters
     ----------
     T : float
@@ -22,7 +22,7 @@ def sim_filtered_brown_noise(T, Fs, f_range, N):
         if None: do not filter
     N : int
         order of filter
-        
+
     Returns
     -------
     brownNf : np.array
@@ -32,7 +32,7 @@ def sim_filtered_brown_noise(T, Fs, f_range, N):
     if f_range is None:
         # Do not filter
         # Generate 1/f^2 noise
-        brownN = simbrown(int(T*Fs))
+        brownN = simbrown(int(T * Fs))
         return brownN
     elif f_range[1] is None:
         # Make filter order odd if necessary
@@ -40,9 +40,9 @@ def sim_filtered_brown_noise(T, Fs, f_range, N):
         if N % 2 == 0:
             print('NOTE: Increased high-pass filter order by 1 in order to be odd')
             N += 1
-            
+
         # Generate 1/f^2 noise
-        brownN = sim_brown_noise(int(T*Fs+N*2))
+        brownN = sim_brown_noise(int(T * Fs + N * 2))
 
         # High pass filter
         taps = signal.firwin(N, f_range[0] / nyq, pass_zero=False)
@@ -52,13 +52,13 @@ def sim_filtered_brown_noise(T, Fs, f_range, N):
     else:
         # Bandpass filter
         # Generate 1/f^2 noise
-        brownN = simbrown(int(T*Fs+N*2))
+        brownN = simbrown(int(T * Fs + N * 2))
         # Filter
         nyq = Fs / 2.
         taps = signal.firwin(N, np.array(f_range) / nyq, pass_zero=False)
         brownNf = signal.filtfilt(taps, [1], brownN)
         return brownNf[N:-N]
-    
+
 
 def sim_brown_noise(N):
     """Simulate a brown noise signal (power law distribution 1/f^2)
@@ -69,7 +69,7 @@ def sim_brown_noise(N):
 def sim_oscillator(N_samples_cycle, N_cycles, rdsym=.5):
     """Simulate a band-pass filtered signal with 1/f^2 
     Input suggestions: f_range=(2,None), Fs=1000, N=1001
-    
+
     Parameters
     ----------
     N_samples_cycle : int
@@ -82,7 +82,7 @@ def sim_oscillator(N_samples_cycle, N_cycles, rdsym=.5):
         =0.5 - symmetric (sine wave)
         <0.5 - shorter rise, longer decay
         >0.5 - longer rise, shorter decay
-        
+
     Returns
     -------
     oscillator : np.array
@@ -93,9 +93,10 @@ def sim_oscillator(N_samples_cycle, N_cycles, rdsym=.5):
     decay_samples = N_samples_cycle - rise_samples
 
     # Make phase array for a single cycle, then repeat it
-    pha_one_cycle = np.hstack([np.linspace(0, np.pi, decay_samples+1), np.linspace(-np.pi, 0, rise_samples+1)[1:-1]])
+    pha_one_cycle = np.hstack([np.linspace(
+        0, np.pi, decay_samples + 1), np.linspace(-np.pi, 0, rise_samples + 1)[1:-1]])
     phase_t = np.tile(pha_one_cycle, N_cycles)
-    
+
     # Transform phase into an oscillator
     oscillator = np.cos(phase_t)
     return oscillator
@@ -104,7 +105,7 @@ def sim_oscillator(N_samples_cycle, N_cycles, rdsym=.5):
 def sim_noisy_oscillator(freq, T, Fs, rdsym=.5, f_hipass_brown=2, SNR=1):
     """Simulate a band-pass filtered signal with 1/f^2 
     Input suggestions: f_range=(2,None), Fs=1000, N=1001
-    
+
     Parameters
     ----------
     freq : float
@@ -120,34 +121,35 @@ def sim_noisy_oscillator(freq, T, Fs, rdsym=.5, f_hipass_brown=2, SNR=1):
         ratio of oscillator power to brown noise power
         >1 - oscillator is stronger
         <1 - noise is stronger
-        
+
     Returns
     -------
     signal : np.array
         oscillator with brown noise
     """
-    
+
     # Determine order of highpass filter (3 cycles of f_hipass_brown)
     N = int(3 * Fs / f_hipass_brown)
     if N % 2 == 0:
         N += 1
-        
+
     # Determine length of signal in samples
-    N_samples = int(T*Fs)
-    
+    N_samples = int(T * Fs)
+
     # Generate filtered brown noise
     brown = sim_filtered_brown_noise(T, Fs, (f_hipass_brown, None), N)
-    
+
     # Generate oscillator
     N_samples_cycle = int(Fs / freq)
     N_cycles = int(np.ceil(N_samples / N_samples_cycle))
     oscillator = sim_oscillator(N_samples_cycle, N_cycles, rdsym=rdsym)
     oscillator = oscillator[:N_samples]
-    
+
     # Normalize brown noise power
     oscillator_power = np.mean(oscillator**2)
     brown_power = np.mean(brown**2)
-    brown = np.sqrt(brown**2 * oscillator_power / (brown_power * SNR)) * np.sign(brown)
+    brown = np.sqrt(brown**2 * oscillator_power /
+                    (brown_power * SNR)) * np.sign(brown)
     # Combine oscillator and noise
     signal = oscillator + brown
     return signal
--- neurodsp/tests/test_sim.py
+++ neurodsp/tests/test_sim.py
@@ -40,16 +40,23 @@ def test_sim_consistent():
     brown = neurodsp.sim_brown_noise(1000)
     brown_filt = neurodsp.sim_filtered_brown_noise(10, 1000, (2, None), 1501)
     osc = neurodsp.sim_oscillator(100, 100, rdsym=.3)
-    noisy_osc = neurodsp.sim_noisy_oscillator(6, 2, 1000, rdsym=.5, f_hipass_brown=2, SNR=2)
+    noisy_osc = neurodsp.sim_noisy_oscillator(
+        6, 2, 1000, rdsym=.5, f_hipass_brown=2, SNR=2)
 
     # Load noise and oscillation
-    brown_true = np.load(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_brown.npy')
-    brown_filt_true = np.load(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_brown_filt.npy')
-    osc_true = np.load(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_osc.npy')
-    noisy_osc_true = np.load(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_noisy_osc.npy')
+    brown_true = np.load(os.path.dirname(
+        neurodsp.__file__) + '/tests/data/sim_brown.npy')
+    brown_filt_true = np.load(os.path.dirname(
+        neurodsp.__file__) + '/tests/data/sim_brown_filt.npy')
+    osc_true = np.load(os.path.dirname(
+        neurodsp.__file__) + '/tests/data/sim_osc.npy')
+    noisy_osc_true = np.load(os.path.dirname(
+        neurodsp.__file__) + '/tests/data/sim_noisy_osc.npy')
 
     # Test consistency between all signals
-    assert np.allclose(np.sum(np.abs(brown-brown_true)), 0, atol=10 ** -5)
-    assert np.allclose(np.sum(np.abs(brown_filt-brown_filt_true)), 0, atol=10 ** -5)
-    assert np.allclose(np.sum(np.abs(osc-osc_true)), 0, atol=10 ** -5)
-    assert np.allclose(np.sum(np.abs(noisy_osc-noisy_osc_true)), 0, atol=10 ** -5)
+    assert np.allclose(np.sum(np.abs(brown - brown_true)), 0, atol=10 ** -5)
+    assert np.allclose(
+        np.sum(np.abs(brown_filt - brown_filt_true)), 0, atol=10 ** -5)
+    assert np.allclose(np.sum(np.abs(osc - osc_true)), 0, atol=10 ** -5)
+    assert np.allclose(
+        np.sum(np.abs(noisy_osc - noisy_osc_true)), 0, atol=10 ** -5)
