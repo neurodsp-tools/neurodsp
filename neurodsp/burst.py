5e054369f9c5694c3d6f6332ecf6a18da6ed2d80
Scott Cole <scott.cole0@gmail.com>
2018-10-30 10:47:28 -0700

    clean bursting module

--- neurodsp/burst.py
+++ neurodsp/burst.py
@@ -8,11 +8,11 @@ from neurodsp import amp_by_time, filt, spectral
 ###################################################################################################
 ###################################################################################################
 
-def detect_bursts(x, Fs, f_range, algorithm, min_osc_periods=3,
-                  dual_thresh=None,
-                  deviation_type='median',
-                  magnitude_type='amplitude',
-                  filter_fn=None, filter_kwargs=None):
+def detect_bursts_2threshold(x, Fs, f_range, dual_thresh,
+                             min_cycles=3,
+                             average_method='median',
+                             magnitude_type='amplitude',
+                             filter_kwargs=None):
     """Detect periods of oscillatory bursting in a neural signal.
 
     Parameters
@@ -21,29 +21,19 @@ def detect_bursts(x, Fs, f_range, algorithm, min_osc_periods=3,
         voltage time series
     Fs : float
         The sampling rate in Hz
-    f_range : (low, high), Hz
-        NOTE: Not relevant in the 'bosc' method
-        frequency range for narrowband signal of interest
-    algorithm : string
-        Name of algorithm to be used.
-        'deviation' : uses multiple of amplitude in frequency range like in
-                      Feingold et al., 2015 (esp. Fig. 4)
-        'fixed_thresh' : uses a given threshold in the same units as 'magnitude'
-                         parameter
-    min_osc_periods : float
+    f_range : tuple of (float, float)
+        frequency range (Hz) for narrowband signal of interest
+    dual_thresh : tuple of (float, float)
+        Low and high threshold values for burst detection
+        Units are normalized by the average signal magnitude
+    min_cycles : float
         minimum burst duration in terms of number of cycles of f_range[0]
-    dual_thresh : (low, high), units depend on other parameters
-        NOTE: Only used when algorithm = 'deviation' or 'fixed_thresh'
-        Threshold values for determining burst
-    deviation_type : string in ('median', 'mean')
+    average_method : string in ('median', 'mean')
         NOTE: Only used when algorithm = 'deviation' or 'fixed_thresh'
         metric to normalize magnitude used for thresholding
     magnitude_type : string in ('power', 'amplitude')
         NOTE: Only used when algorithm = 'deviation' or 'fixed_thresh'
         metric of magnitude used for thresholding
-    filter_fn : filter function with required inputs (x, f_range, Fs, rmv_edge)
-        NOTE: Only used when algorithm = 'deviation' or 'fixed_thresh'
-        function to use to filter original time series, x
     filter_kwargs : dict
         NOTE: Only used when algorithm = 'deviation' or 'fixed_thresh'
         keyword arguments to the filter_fn
@@ -55,138 +45,41 @@ def detect_bursts(x, Fs, f_range, algorithm, min_osc_periods=3,
         True indicates that a burst was detected at that sample, otherwise False.
     """
 
-    if algorithm in ['deviation', 'fixed_thresh']:
+    # Set default filtering parameters
+    if filter_kwargs is None:
+        filter_kwargs = {}
 
-        # Set default filtering parameters
-        if filter_kwargs is None:
-            filter_kwargs = {}
+    if len(dual_thresh) != 2:
+        raise ValueError(
+            "Invalid number of elements in 'dual_thresh' parameter")
 
-        # Assure dual_thresh has input
-        if dual_thresh is None:
-            raise ValueError(
-                'Need to specify dual magnitude thresholds for this algorithm')
+    # Compute amplitude time series
+    x_magnitude = amp_by_time(x, Fs, f_range, filter_kwargs=filter_kwargs,
+                              remove_edge_artifacts=False)
 
-        # Process deviation_type kwarg
-        if deviation_type not in ['median', 'mean']:
-            raise ValueError(
-                "Invalid 'baseline' parameter. Must be 'median' or 'mean'")
-
-        # Compute amplitude time series
-        x_amplitude = amp_by_time(
-            x, Fs, f_range, filter_fn=filt.filter, filter_kwargs=filter_kwargs,
-            remove_edge_artifacts=False)
-
-        # Set magnitude as power or amplitude
-        if magnitude_type == 'power':
-            x_magnitude = x_amplitude**2  # np.power faster?
-        elif magnitude_type == 'amplitude':
-            x_magnitude = x_amplitude
-        else:
-            raise ValueError("Invalid 'magnitude' parameter")
-
-        # Rescale magnitude by median or mean
-        # If 'fixed_thresh', x_magnitude is unchanged
-        if algorithm == 'deviation':
-            # Calculate normalized magnitude
-            if deviation_type == 'median':
-                x_magnitude = x_magnitude / np.median(x_magnitude)
-            elif deviation_type == 'mean':
-                x_magnitude = x_magnitude / np.mean(x_magnitude)
-
-        if len(dual_thresh) != 2:
-            raise ValueError(
-                "Invalid number of elements in 'dual_thresh' parameter")
-
-        # Identify time periods of oscillation using the 2 thresholds
-        is_burst = _2threshold_split(x_magnitude, dual_thresh[1], dual_thresh[0])
-
+    # Set magnitude as power or amplitude
+    if magnitude_type == 'power':
+        x_magnitude = x_magnitude**2
+    elif magnitude_type == 'amplitude':
+        pass
     else:
-        raise ValueError("Invalid 'algorithm' parameter")
+        raise ValueError("Invalid input for 'magnitude_type'")
 
-    # Remove short time periods of oscillation
-    min_period_length = int(np.ceil(min_osc_periods * Fs / f_range[0]))
+    # Calculate normalized magnitude
+    if average_method == 'median':
+        x_magnitude = x_magnitude / np.median(x_magnitude)
+    elif average_method == 'mean':
+        x_magnitude = x_magnitude / np.mean(x_magnitude)
+    else:
+        raise ValueError("Invalid input for 'average_method'")
+
+    # Identify time periods of bursting using the 2 thresholds
+    is_burst = _2threshold_split(x_magnitude, dual_thresh[1], dual_thresh[0])
+
+    # Remove bursts detected that are too short
+    min_period_length = int(np.ceil(min_cycles * Fs / f_range[0]))
     is_burst = _rmv_short_periods(is_burst, min_period_length)
     return is_burst
-
-
-def detect_bursts_bosc(x, Fs, f_oi, f_range_slope, f_slope_excl,
-                       percentile_thresh=None, plot_slope_fit=None):
-    """Detect bursts of oscillations using the Better OSCillation detection algorithm.
-
-    Briefly, we estimate the background 1/f process of the signal, and use this to
-    determine a power threshold at our frequency of interest.
-
-    Rather than looking at the whole frequency band, we only look at a single frequency value.
-    This frequency value is chosen as the middle of the frequency band defined,
-    rounded to the nearest integer, rounded down.
-
-    Note that the BOSC paper recommends a minimum of 3 cycles to be identified as a burst.
-
-    Parameters
-    ----------
-    x : 1d array-like
-        Voltage time series
-    Fs : float
-        The sampling rate. Also used for the window size to get frequency spacing of 1Hz
-    f_oi : int
-        frequency of the oscillation of interest (Hz)
-    f_range_slope : (low, high), Hz
-        frequency range over which to estimate slope
-    f_slope_excl : (low, high), Hz'
-        Frequency range to ignore in slope fit. This should contain the
-        frequency of the oscillation of interest
-    percentile_thresh : int (0 to 1)
-        the probability of the chi-square distribution at which to cut off oscillation
-    plot_slope_fit : bool
-        whether to plot the fitted slope to the power spectrum
-
-    Returns
-    -------
-    isosc : 1d array-like
-        binary time series. 1 = in oscillation; 0 = not in oscillation
-        if return_oscbounds is true: this is 2 lists with the start and end burst samples
-
-    Reference
-    ---------
-    Implements algorithm as described in Whitten et al., 2011, NeuroImage.
-    """
-
-    # Set default threshold of chi2 distribution
-    if percentile_thresh is None:
-        percentile_thresh = .95
-
-    # Set default sloep plotting
-    if plot_slope_fit is None:
-        plot_slope_fit = False
-
-    # Compute Morlet Transform with 6 cycles
-    f0s = np.arange(f_range_slope[0], f_range_slope[1])
-    mwt = spectral.morlet_transform(x, f0s, Fs, w=6)
-    mwt_power = np.abs(mwt**2)
-
-    if sum(f_oi == f0s) != 1:
-        raise ValueError("The frequency of interest, f_oi, must be within\
-                          the frequency range used to fit the slope")
-
-    # Compute average spectrum, and fit slope to it
-    avg_spectrum = np.mean(mwt_power, axis=1)
-    slope, offset = _fit_slope(f0s, avg_spectrum, f_range_slope,
-                               fit_excl=f_slope_excl, plot_fit=plot_slope_fit)
-
-    # Compute background power at frequency of interest, and the power
-    # threshold
-    f_oi_background_power = 10**(np.log10(f_oi) * slope + offset)
-    power_thresh = stats.chi2.ppf(
-        percentile_thresh, 2, scale=f_oi_background_power / 2)
-
-    # Determine periods that are oscillating
-    power_ts = mwt_power[f0s == f_oi][0]
-    isosc = power_ts > power_thresh
-
-    # Remove bursts less than 3 cycles in duration
-    min_period_length = int(3 * Fs / f_oi)
-    isosc_noshort = _rmv_short_periods(isosc, min_period_length)
-    return isosc_noshort
 
 
 def get_stats(bursting, Fs):
@@ -195,7 +88,8 @@ def get_stats(bursting, Fs):
     Parameters
     ----------
     bursting : array-like 1d
-        binary time series, output of detect_bursts()
+        Boolean indication of where bursts are present in the input signal.
+        Output of detect_bursts_2threshold()
     Fs : float
         The sampling rate in Hz
 
@@ -224,13 +118,13 @@ def get_stats(bursting, Fs):
     # duration of each burst
     durations = (ends - starts) / Fs
 
-    ret_dict = {'N_bursts': len(starts),
-                'duration_mean': np.mean(durations),
-                'duration_std': np.std(durations),
-                'percent_burst': np.sum(bursting) / len(bursting),
-                'rate': len(starts) / tot_time}
+    stats_dict = {'N_bursts': len(starts),
+                  'duration_mean': np.mean(durations),
+                  'duration_std': np.std(durations),
+                  'percent_burst': 100 * np.sum(bursting) / len(bursting),
+                  'bursts_per_second': len(starts) / tot_time}
 
-    return ret_dict
+    return stats_dict
 
 
 def _2threshold_split(x, thresh_hi, thresh_lo):
@@ -308,62 +202,3 @@ def _rmv_short_periods(x, N):
     for osc in range(len(osc_starts_long)):
         is_osc[osc_starts_long[osc]:osc_ends_long[osc]] = 1
     return is_osc
-
-
-def _fit_slope(freq, psd, fit_frange, fit_excl=None, plot_fit=False):
-    """Fit PSD with straight line in log-log domain over the specified frequency range.
-
-    Parameters
-    ----------
-    freq : array_like, 1d
-        Frequency axis of PSD
-    psd : array_like, 1d
-        PSD to be fit over
-    fit_frange : tuple, (start, end), Hz
-        Frequency range to be fit over, in Hz, inclusive on both ends.
-    fit_excl : list of tuples, [(start, end), (start, end), ...], Hz, optional
-        Frequency ranges to be excluded from fit. Each element in list describes
-        the start and end of an exclusion zone.
-    plot_fit : bool, optional
-        If True, the PSD is plotted, along with the actual fitted PSD (excluding exclusion freqs),
-        as well as the fitted line itself. Defaults to False.
-
-    Returns
-    -------
-    slope : float
-        Slope of loglog fitted line, m in y = mx+b
-    offset : float
-        Offset of loglog fitted line, b in y = mx+b
-    """
-
-    # make a mask for included and excluded frequency regions
-    fmask = np.zeros_like(freq)
-    # get freq indices within the fit frequencies
-    fmask[np.logical_and(freq >= fit_frange[0], freq <= fit_frange[1])] = 1
-    # discard freq indices within the exclusion frequencies
-    if fit_excl is not None:
-        # if a tuple is given, convert it to a list
-        if isinstance(fit_excl, tuple):
-            fit_excl = [fit_excl]
-
-        for exc_frange in fit_excl:
-            fmask[np.logical_and(freq >= exc_frange[0],
-                                 freq <= exc_frange[1])] = 0
-
-    # grab the psd and freqs to be fit over
-    logf = np.log10(freq[fmask == 1])
-    logpsd = np.log10(psd[fmask == 1])
-
-    # fit line
-    slope, offset = np.polyfit(logf, logpsd, deg=1)
-
-    if plot_fit:
-        plt.figure(figsize=(5, 5))
-        plt.plot(np.log10(freq), np.log10(psd), label='Whole PSD')
-        plt.plot(logf, logpsd, '-o', label='Fitted PSD', alpha=0.4)
-        plt.plot(logf, logf * slope + offset, '-k', label='Fit Line', lw=3)
-        plt.legend()
-        plt.xlabel('Log10 Frequency (Hz)', fontsize=15)
-        plt.ylabel('Log10 Power (V^2/Hz)', fontsize=15)
-
-    return slope, offset
--- neurodsp/tests/test_burst.py
+++ neurodsp/tests/test_burst.py
@@ -4,13 +4,12 @@ Test burst detection functions
 """
 
 import os
-import pytest
 import numpy as np
 import neurodsp
 from neurodsp.tests import _load_example_data
 
 
-def test_detect_bursts_consistent():
+def test_detect_bursts_2threshold():
     """
     Confirm consistency in burst detection results on a generated neural signal
     """
@@ -18,20 +17,12 @@ def test_detect_bursts_consistent():
     x = _load_example_data(data_idx=1)
     Fs = 1000
     f_range = (13, 30)
-    f_oi = np.floor(np.mean(f_range))
-    f_range_slope = (3, 50)
-    f_slope_excl = f_range
 
     # Load past burst findings
-    bursting_true_deviation = np.load(os.path.dirname(neurodsp.__file__) +
-                                      '/tests/data/sample_data_1_burst_deviation.npy')
-    bursting_true_bosc = np.load(os.path.dirname(neurodsp.__file__) +
-                                 '/tests/data/sample_data_1_burst_bosc.npy')
+    bursting_true = np.load(os.path.dirname(neurodsp.__file__) +
+                            '/tests/data/sample_data_1_burst_deviation.npy')
 
     # Detect bursts with different algorithms
-    bursting_deviation = neurodsp.detect_bursts(x, Fs, f_range, 'deviation',
-                                                dual_thresh=(0.9, 2.0))
-    bursting_bosc = neurodsp.detect_bursts_bosc(x, Fs, f_oi, f_range_slope, f_slope_excl)
+    bursting = neurodsp.detect_bursts_2threshold(x, Fs, f_range, (0.9, 2))
 
-    assert np.isclose(np.sum(bursting_deviation - bursting_true_deviation), 0)
-    assert np.isclose(np.sum(bursting_bosc - bursting_true_bosc), 0)
+    assert np.isclose(np.sum(bursting - bursting_true), 0)
