1c7b53aa58d25638e9956f4e545b481071150055
Scott Cole <scott.cole0@gmail.com>
2018-11-01 09:51:39 -0700

    tom wants plts with an s

--- neurodsp/filt.py
+++ neurodsp/filt.py
@@ -6,7 +6,7 @@ import numpy as np
 import scipy as sp
 from scipy import signal
 
-from neurodsp.plt.filt import plot_frequency_response
+from neurodsp.plts.filt import plot_frequency_response
 
 ###################################################################################################
 ###################################################################################################
