9cfe6c55a35e730264b5cdac6f5dc6a4c1381e66
rdgao <r.dg.gao@gmail.com>
2018-11-05 01:50:38 -0800

    added tests for morlet_transform, and updated spectral tutorial

--- neurodsp/tests/test_spectral.py
+++ neurodsp/tests/test_spectral.py
@@ -120,3 +120,20 @@ def test_rotatepsd():
 
 
     assert np.allclose(spectrum_rot - spectrum_test, 0, atol=10 ** -5)
+
+
+def test_morlet_transform():
+
+    data_idx = 1
+    sig = _load_example_data(data_idx=data_idx)
+    fs = 1000.
+
+    morlet_freqs = np.logspace(0,7,29, base=2)
+    mwt = spectral.morlet_transform(sig, morlet_freqs ,fs)
+
+    # load "ground truth" MWT
+    gt_mwt = np.load(os.path.dirname(neurodsp.__file__) +
+                     '/tests/data/mwt.npy')
+
+    # compute the difference
+    assert np.allclose(np.sum(np.abs(gt_mwt - mwt)), 0, atol=10 ** -5)
