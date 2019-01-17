78be5ad47a96615d0f9d2d293e310f36b428a878
rdgao <r.dg.gao@gmail.com>
2019-01-16 16:30:05 -0800

    pep8 shit

--- neurodsp/tests/test_sim.py
+++ neurodsp/tests/test_sim.py
@@ -99,8 +99,9 @@ def test_sim_oscillator():
 def test_sim_noisy_oscillator():
     np.random.seed(0)
     osc = sim_noisy_oscillator(n_seconds, fs, freq, 'filtered_powerlaw',
-        {'exponent':exponent, 'f_range':f_range_filter, 'filter_order':filter_order},
-            ratio_osc_var=ratio_osc_var)
+                               {'exponent': exponent, 'f_range': f_range_filter,
+                                   'filter_order': filter_order},
+                               ratio_osc_var=ratio_osc_var)
 
     # np.save(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_noisy_osc.npy', osc)
     osc_true = np.load(os.path.dirname(
@@ -120,9 +121,10 @@ def test_sim_bursty_oscillator():
 def test_sim_noisy_bursty_oscillator():
     np.random.seed(0)
     osc = sim_noisy_bursty_oscillator(n_seconds, fs, freq, 'filtered_powerlaw',
-        {'exponent':exponent, 'f_range':f_range_filter, 'filter_order':filter_order},
-            rdsym=.5, ratio_osc_var=1, prob_enter_burst=.2, prob_leave_burst=.2,
-            cycle_features=None, return_components=False, return_cycle_df=False)
+                                      {'exponent': exponent, 'f_range': f_range_filter,
+                                          'filter_order': filter_order},
+                                      rdsym=.5, ratio_osc_var=1, prob_enter_burst=.2, prob_leave_burst=.2,
+                                      cycle_features=None, return_components=False, return_cycle_df=False)
 
     # np.save(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_noisy_bursty_osc.npy', osc)
     osc_true = np.load(os.path.dirname(
@@ -145,13 +147,13 @@ def test_sim_make_synaptic_kernel():
 
     # smoke test that valid parameter configurations do not return negative values
     t_ker, fs, tau_r, tau_d = 1., 1000., 0., 0.02
-    assert np.all(make_synaptic_kernel(t_ker, fs, tau_r, tau_d)>=0.)
+    assert np.all(make_synaptic_kernel(t_ker, fs, tau_r, tau_d) >= 0.)
 
     t_ker, fs, tau_r, tau_d = 2., 2000., 0.005, 0.02
-    assert np.all(make_synaptic_kernel(t_ker, fs, tau_r, tau_d)>=0.)
+    assert np.all(make_synaptic_kernel(t_ker, fs, tau_r, tau_d) >= 0.)
 
     t_ker, fs, tau_r, tau_d = 1., 1000., 0.02, 0.02
-    assert np.all(make_synaptic_kernel(t_ker, fs, tau_r, tau_d)>=0.)
+    assert np.all(make_synaptic_kernel(t_ker, fs, tau_r, tau_d) >= 0.)
 
 
 def test_sim_synaptic_noise():
@@ -174,7 +176,7 @@ def test_sim_ou_process():
 
 def test_sim_jittered_oscillator():
     np.random.seed(0)
-    jittered_osc = sim_jittered_oscillator(2, 1000, 20, 0.00, ('gaussian',0.01))
+    jittered_osc = sim_jittered_oscillator(2, 1000, 20, 0.00, ('gaussian', 0.01))
     # np.save(os.path.dirname(neurodsp.__file__) + '/tests/data/sim_jittered_oscillator.npy', jittered_osc)
     jittered_osc_true = np.load(os.path.dirname(
         neurodsp.__file__) + '/tests/data/sim_jittered_oscillator.npy')
