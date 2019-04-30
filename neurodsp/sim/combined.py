96e0a7454eebcc95e3c9dfdff6b3682bd23442af
TomDonoghue <tdonoghue@ucsd.edu>
2019-04-30 14:40:52 -0700

    Update approach for sim_combined to allow for simualting multiple params from same func

--- neurodsp/sim/combined.py
+++ neurodsp/sim/combined.py
@@ -31,7 +31,12 @@ def sim_combined(n_seconds, fs, simulations, variances=1):
         Simulated combined signal.
     """
 
-    if not (variances == 1 or len(variances) == len(simulations)):
+    # Check how simulations are specified, in terms of number of parameter sets
+    n_sims = sum([1 if isinstance(params, dict) else len(params) \
+        for params in simulations.values()])
+
+    # Check that the variance definition matches the number of components specified
+    if not (variances == 1 or len(variances) == n_sims):
         raise ValueError('Simulations and proportions lengths do not match.')
 
     # Collect the sim function to use, and repeat variance if is set to 1
@@ -39,9 +44,20 @@ def sim_combined(n_seconds, fs, simulations, variances=1):
                    for name, params in simulations.items()}
     variances = repeat(variances) if isinstance(variances, int) else variances
 
-    # Simulate each component, specifying variance, and combine them
-    components = [func(n_seconds, fs, **params, variance=variance) for \
-        (func, params), variance in zip(simulations.items(), variances)]
+    # Simulate each component, specifying the variance for each
+    components = []
+    for (func, params), variance in zip(simulations.items(), variances):
+
+        # If list, params should be a list of separate parameters for each fucntion call
+        if isinstance(params, list):
+            components.extend([func(n_seconds, fs, **cur_params, variance=variance) \
+                for cur_params in params])
+
+        # Otherwise, params should be a dictionary of parameters for single call
+        else:
+            components.append(func(n_seconds, fs, **params, variance=variance))
+
+    # Combine total signal across all simulated components
     sig = np.sum(components, axis=0)
 
     return sig
--- neurodsp/tests/test_sim_combined.py
+++ neurodsp/tests/test_sim_combined.py
@@ -14,3 +14,9 @@ def test_sim_combined():
                    'sim_powerlaw' : {}}
     variances = [1, 0.5]
     out = sim_combined(N_SECONDS, FS, simulations, variances)
+
+    # Test case with mutliple uses of same function
+    simulations = {'sim_oscillation' : [{'freq' : 10}, {'freq' : 10}],
+                   'sim_powerlaw' : {}}
+    variances = [0.5, 0.5, 1]
+    out = sim_combined(N_SECONDS, FS, simulations, variances)
