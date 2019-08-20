08ba57f2bf824f6f81a2da0da7fbf12cdbe38aae
TomDonoghue <tdonoghue@ucsd.edu>
2019-08-19 23:10:50 -0700

    Split out sim tutorials

---
+++ tutorials/sim/plot_SimulateAperiodic.py
@@ -0,0 +1,160 @@
+"""
+Simulating Aperiodic Signals
+============================
+
+Simulate aperiodic, or 1/f-like, signals.
+
+This tutorial covers :mod:`neurodsp.sim.aperiodic`
+"""
+
+###################################################################################################
+
+from neurodsp import sim, spectral
+from neurodsp.utils import create_times
+
+from neurodsp.plts.spectral import plot_power_spectra
+from neurodsp.plts.time_series import plot_time_series
+
+###################################################################################################
+
+# Set the random seed, for consistency simulating data
+sim.set_random_seed(0)
+
+# Set some general settings, to be used across all simulations
+fs = 1000
+n_seconds = 10
+
+###################################################################################################
+#
+# Simulate 1/f Activity
+# ---------------------
+#
+# Often, we want to simulate noise that is comparable to what we see in neural recordings.
+#
+# Neural signals display 1/f like activity, whereby power decreases linearly across
+# increasing frequencies, when plotting in log-log.
+#
+# Let's start with a powerlaw signal, specifically a brown noise process, or a signal
+# for which the power spectrum is distributed as 1/f^2.
+#
+
+###################################################################################################
+
+# Setting for the simulation
+exponent = -2
+
+# Simulate powerlaw activity, specifically brown noise
+times = create_times(n_seconds, fs)
+br_noise = sim.sim_powerlaw(n_seconds, fs, exponent)
+
+###################################################################################################
+
+# Plot the simulated data, in the time domain
+plot_time_series(times, br_noise)
+
+###################################################################################################
+
+# Plot the simulated data, in the frequency domain
+freqs, psd = spectral.compute_spectrum(br_noise, fs)
+plot_power_spectra(freqs, psd)
+
+###################################################################################################
+#
+# Simulate Filtered 1/f Activity
+# ------------------------------
+#
+# The powerlaw simulation function is also integrated with a filter. This can be useful
+# if one wants to filter out the slow frequencies, as is often done with neural signals,
+# to remove the very slow drifts that we see in the pure 1/f simulations.
+#
+# To filter a simulated powerlaw signal, simply pass in a filter range, and the filter will
+# be applied to the simulated data before being returned. Here we will apply a high-pass filter.
+#
+# We can see that the resulting signal has much less low-frequency drift than the first one.
+#
+
+###################################################################################################
+
+# Simulate highpass-filtered brown noise with a 1Hz cutoff frequency
+f_hipass_brown = 1
+brown_filt = sim.sim_powerlaw(n_seconds, fs, exponent, f_range=(f_hipass_brown, None))
+
+###################################################################################################
+
+# Plot the simulated data, in the time domain
+plot_time_series(times, brown_filt)
+
+###################################################################################################
+
+# Plot the simulated data, in the frequency domain
+freqs, psd = spectral.compute_spectrum(brown_filt, fs)
+plot_power_spectra(freqs, psd)
+
+###################################################################################################
+#
+# Note: the :func:`sim_powerlaw` function can simualate power law exponents,
+# such as pink noise (-1), or any other exponent.
+#
+
+###################################################################################################
+#
+# Random Walk Activity
+# --------------------
+#
+# We can also simulate an Ornstein-Uhlenbeck process, which is a random walk process with memory.
+#
+
+###################################################################################################
+
+# Simulate aperiodic signals from a random walk process
+rw_noise = sim.sim_random_walk(n_seconds, fs)
+
+###################################################################################################
+
+# Plot the simulated data, in the time domain
+plot_time_series(times, rw_noise, title='RW Process')
+
+###################################################################################################
+#
+# Simulate Synaptic Activity
+# --------------------------
+#
+# Another model for simulating aperiodic, neurally plausible activity, is to simulate
+# synaptic current activity, as a Lorentzian function.
+#
+# The synaptic current moedel is a poisson activity convoled with exponential kernels
+# that mimic the shape of post-synaptic potentials.
+#
+# For more details on the usage of such models for simulating neural signals,
+# see Destexhe et al., 2001 and/or Gao et al., 2017.
+#
+
+###################################################################################################
+
+# Simulate aperiodic activity from the synaptic kernel model
+syn_noise = sim.sim_synaptic_current(n_seconds, fs)
+
+###################################################################################################
+
+# Plot the simulated data, in the time domain
+plot_time_series(times, syn_noise, title='Synaptic Activity')
+
+###################################################################################################
+#
+# Both the random walk, and synaptic model produce 1/f scaling in higher frequencies with a
+# fixed exponent of -2, as we can see in the power spectra plot below.
+#
+
+###################################################################################################
+
+# Plot the simulated data, in the frequency domain
+freqs, rw_psd = spectral.compute_spectrum(rw_noise, fs)
+freqs, syn_psd = spectral.compute_spectrum(syn_noise, fs)
+
+plot_power_spectra(freqs, [rw_psd, syn_psd], ['RW', 'Synaptic'])
+
+###################################################################################################
+#
+# Sphinx settings:
+# sphinx_gallery_thumbnail_number = 3
+#
--- tutorials/sim/plot_SimulatePeriodic.py
+++ tutorials/sim/plot_SimulatePeriodic.py
@@ -1,17 +1,15 @@
 """
-Simulating Signals
-==================
+Simulating Periodic Signals
+===========================
 
-Simulate periodic (oscillatory) and aperiodic (1/f-like) signals.
+Simulate periodic, or oscillatory, signals.
 
-This tutorial primarily covers :mod:`neurodsp.sim`.
+This tutorial covers :mod:`neurodsp.sim.periodic`.
 """
 
 ###################################################################################################
 
-import numpy as np
-
-from neurodsp import spectral, sim
+from neurodsp import sim, spectral
 from neurodsp.utils import create_times
 
 from neurodsp.plts.spectral import plot_power_spectra
@@ -20,204 +18,99 @@ from neurodsp.plts.time_series import plot_time_series
 ###################################################################################################
 
 # Set the random seed, for consistency simulating data
-np.random.seed(0)
+sim.set_random_seed(0)
 
-###################################################################################################
-# Simulate brown noise
-# --------------------
-#
-# Often, we want to simulate noise that is comparable to what we see in neural recordings.
-#
-# Brown noise is a good proxy for that because, like neural signals,
-# power decreases roughly linearly with frequency.
-#
-
-###################################################################################################
-
-# Simulate brown noise
-n_seconds = 10
+# Set some general settings, to be used across all simulations
 fs = 1000
-exponent = -2
+
+###################################################################################################
+#
+# Simulate a Stationary Oscillation
+# ---------------------------------
+#
+# Let's start by simulating an oscillation - in this case, a simple, sinuisoidal oscillation.
+#
+
+###################################################################################################
+
+# Simulation settings
+n_seconds = 1
+osc_freq = 6.6
+
+# Simulate a sinusoidal oscillation
+osc_sine = sim.sim_oscillation(n_seconds, fs, osc_freq, cycle='sine')
+
+# Create a times vector for our simulation
 times = create_times(n_seconds, fs)
-br_noise = sim.sim_powerlaw(n_seconds, fs, exponent)
+
+# Plot the simulated data, in the time domain
+plot_time_series(times, osc_sine)
+
+###################################################################################################
+#
+# Cycle Kernels
+# -------------
+#
+# To simulate oscillations, we can use a sinuisoidal kernel, as above, or any of a selection
+# of other cycle kernals, that represent different shapes and properties that may be useful
+# to simulate different aspects of periodic neural activity.
+#
+# Cycle kernel options include:
+#
+# - sine: a sine wave cycle
+# - asine: an asymmetric sine wave
+# - sawtooth: a sawtooth wave
+# - gaussian: a gaussian cycle
+# - exp: a cycle with exponential decay
+# - 2exp: a cycle with exponential rise and decay
+#
+
+###################################################################################################
+#
+# Simulate a Shapely Oscillation
+# ------------------------------
+#
+# Next let's simulate an aymmetric oscillation, using the `asine` cycle kernel,
+# which stands for 'asymmetric sinuisoidal'. Using the `asine` kernel, you
+# can simulate arbitrary rise-decay symmetry of oscillations.
+#
+# We'll plot it over our original sinuisoidal oscillation, so we can compare them.
+#
+
+###################################################################################################
+
+# Simulate a shape-y oscillations
+osc_shape = sim.sim_oscillation(n_seconds, fs, osc_freq, cycle='asine', rdsym=.2)
 
 ###################################################################################################
 
 # Plot the simulated data, in the time domain
-plot_time_series(times, br_noise)
+plot_time_series(times, [osc_sine, osc_shape], ['rdsym='+str(.5), 'rdsym='+str(.3)])
+
+###################################################################################################
+#
+# We can also compare these signals in the frequency.
+#
+# Notice that the asymmetric oscillation has strong harmonics resulting from the
+# non-sinusoidal nature of the oscillation.
+#
 
 ###################################################################################################
 
 # Plot the simulated data, in the frequency domain
-freqs, psd = spectral.compute_spectrum(br_noise, fs)
-plot_power_spectra(freqs, psd)
+freqs_sine, psd_sine = spectral.compute_spectrum(osc_sine, fs)
+freqs_shape, psd_shape = spectral.compute_spectrum(osc_shape, fs)
+
+plot_power_spectra([freqs_sine, freqs_shape], [psd_sine, psd_shape])
 
 ###################################################################################################
 #
-# Simulate filtered brown noise
+# Simulate a Bursty Oscillation
 # -----------------------------
 #
-# However, brown noise has a lot of power in very slow frequnecies,
-# whereas these slow frequencies are often not present or filtered out
-# in neural signals. Therefore, we may desire our brown noise to be
-# high-pass filtered. See that the resulting signal has much less
-# low-frequency drift.
-#
-# Note this might not be ideal because it creates an "oscillation" at
-# the cutoff frequency.
-#
-
-###################################################################################################
-
-# Simulate highpass-filtered brown noise with a 1Hz cutoff frequency
-f_hipass_brown = 1
-brown_filt = sim.sim_powerlaw(n_seconds, fs, exponent, f_range=(f_hipass_brown, None))
-
-###################################################################################################
-
-# Plot the simulated data, in the time domain
-plot_time_series(times, brown_filt)
-
-###################################################################################################
-
-# Plot the simulated data, in the frequency domain
-freqs, psd = spectral.compute_spectrum(brown_filt, fs)
-plot_power_spectra(freqs, psd)
-
-###################################################################################################
-#
-# Note: the :func:`sim_variable_powerlaw` and :func:`sim_filtered_noise` functions can
-# support arbitrary power law exponents, such as pink noise (-1).
-#
-
-###################################################################################################
-#
-# Other 1/f-like noises
-# ---------------------
-#
-# We can also simulate an Ornstein-Uhlenbeck process (random walk with memory),
-# as well as 'synaptic (Lorentzian) noise', which is white noise convolved
-# with an exponentially-decaying kernel mimicking the shape of a post-synaptic
-# potential.
-#
-# Both of these noise models produce 1/f scaling in higher frequencies with a
-# fixed exponent of -2. For more details on their usage in simulating neural
-# signals, please see Destexhe et al., 2001 and Gao et al., 2017.
-#
-
-###################################################################################################
-
-# Simulate aperiodic signals from a random walk process & synaptic noise model
-rw_noise = sim.sim_random_walk(n_seconds, fs)
-syn_noise = sim.sim_synaptic_current(n_seconds, fs)
-
-###################################################################################################
-
-# Plot the simulated data, in the time domain
-plot_time_series(times, rw_noise, title='RW Process')
-plot_time_series(times, syn_noise, title='Synaptic Noise')
-
-###################################################################################################
-
-# Plot the simulated data, in the frequency domain
-freqs, rw_psd = spectral.compute_spectrum(rw_noise, fs)
-freqs, syn_psd = spectral.compute_spectrum(syn_noise, fs)
-
-plot_power_spectra(freqs, [rw_psd, syn_psd], ['RW', 'Synaptic'])
-
-###################################################################################################
-#
-# Simulate a stationary oscillator
-# --------------------------------
-#
-# In addition to noise, you may also want to simulate an oscillatory process.
-# We can do that with neurodsp, with arbitrary rise-decay symmetry.
-#
-
-###################################################################################################
-
-# Simulate symmetric oscillator
-n_seconds = 1
-fs = 1000
-osc_freq = 6.6
-osc_a = sim.sim_oscillation(n_seconds, fs, osc_freq, cycle='asine', rdsym=.5)
-osc_b = sim.sim_oscillation(n_seconds, fs, osc_freq, cycle='asine', rdsym=.2)
-
-# HACK: REMOVE WHEN SIM UPDATED
-osc_a = osc_a[0:n_seconds*fs]
-osc_b = osc_b[0:n_seconds*fs]
-
-times = create_times(n_seconds, fs)
-
-###################################################################################################
-
-# Plot the simulated data, in the time domain
-plot_time_series(times, [osc_a, osc_b], ['rdsym='+str(.5), 'rdsym='+str(.3)])
-
-###################################################################################################
-
-# Plot the simulated data, in the frequency domain
-freqs_a, psd_a = spectral.compute_spectrum(osc_a, fs)
-freqs_b, psd_b = spectral.compute_spectrum(osc_b, fs)
-
-plot_power_spectra([freqs_a, freqs_b], [psd_a, psd_b])
-
-###################################################################################################
-#
-# Notice the strong harmonics resulting from the non-sinusoidal nature of
-# the red oscillation.
-#
-
-###################################################################################################
-#
-# Simulate a noisy oscillator
-# ---------------------------
-#
-# In order to simulate a signal that looks more like a brain signal,
-# you may want to simulate both an oscillation and noise. We also do this
-# with neurodsp bycan combining all of the above, with control over the
-# power ratio between oscillation and noise.
-#
-# You can use any of the above noise models to generate the background
-# noise by passing in its name (str argument) and the required model
-# parameters as a dictionary.
-
-###################################################################################################
-
-# Settings for simulation
-n_seconds = 5
-fs = 1000
-
-components = {'sim_synaptic_current' : {'n_neurons':1000, 'firing_rate':2, 't_ker':1.0,
-                                        'tau_r':0.002, 'tau_d':0.02},
-              'sim_oscillation' : {'freq' : 8}}
-
-###################################################################################################
-
-# Simulate an oscillation over an aperiodic component & associated times vector
-times = create_times(n_seconds, fs)
-signal = sim.combined.sim_combined(n_seconds, fs, components)
-
-###################################################################################################
-
-# Plot the simulated data, in the time domain
-samps_plot = np.arange(1000)
-plot_time_series(times[samps_plot], signal[samps_plot], xlim=[0, times[samps_plot[-1]]])
-
-###################################################################################################
-
-# Plot the simulated data, in the frequency domain
-freqs, psd = spectral.compute_spectrum(signal, fs)
-plot_power_spectra(freqs, psd)
-
-###################################################################################################
-#
-# Simulate a Bursting Oscillation
-# -------------------------------
-#
-# Sometimes we want to study oscillations that come and go, so it can be
-# useful to simulate oscillations with this property. We can do this by
-# controlling the probability that a burst will start or stop with each new cycle.
+# Sometimes we want to study oscillations that come and go, so it can be useful to simulate
+# oscillations with this property. We can do this by controlling the probability that a burst
+# will start or stop with each new cycle.
 #
 
 ###################################################################################################
@@ -225,7 +118,6 @@ plot_power_spectra(freqs, psd)
 # Settings for simulation
 osc_freq = 30
 n_seconds = 3
-fs = 1000
 enter_burst = .1
 leave_burst = .1
 
@@ -261,7 +153,7 @@ plot_time_series(times, osc, xlim=[0, n_seconds])
 
 ###################################################################################################
 #
-# We can increase the number of bursts by increasing the probability to enter a burst.
+# We can increase the number of bursts by increasing the probability to enter a burst
 #
 
 ###################################################################################################
@@ -280,39 +172,6 @@ plot_time_series(times, osc, xlim=[0, n_seconds])
 
 ###################################################################################################
 #
-# As with the stationary oscillator, we can also simulate a bursting
-# oscillator with brown noise.
-#
-
-###################################################################################################
-
-# Settings for simulation
-n_seconds = 5
-fs = 1000
-components = {'sim_synaptic_current' : {'n_neurons':1000, 'firing_rate':2,
-                                        't_ker':1.0, 'tau_r':0.002, 'tau_d':0.02},
-              'sim_bursty_oscillation' : {'freq' : 10}}
-
-
-###################################################################################################
-
-# Simulate a bursty oscillation combined with aperiodic activity
-times = create_times(n_seconds, fs)
-osc = sim.combined.sim_combined(n_seconds, fs, components)
-
-###################################################################################################
-
-# Plot the simulated data, in the time domain
-plot_time_series(times, osc, xlim=[0, n_seconds])
-
-###################################################################################################
-
-# Plot the simulated data, in the frequency domain
-freqs, psd = spectral.compute_spectrum(osc, fs)
-plot_power_spectra(freqs, psd)
-
-###################################################################################################
-#
 # Sphinx settings:
-# sphinx_gallery_thumbnail_number = 15
+# sphinx_gallery_thumbnail_number = 1
 #
