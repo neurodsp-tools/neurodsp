f61f339dbc1782f7c5cd6cee6e3a5cd4758622bb
TomDonoghue <tdonoghue@ucsd.edu>
2019-10-09 15:41:23 -0700

    update DFA with review comments & associated updates

--- neurodsp/aperiodic/dfa.py
+++ neurodsp/aperiodic/dfa.py
@@ -3,6 +3,8 @@
 import numpy as np
 import scipy as sp
 
+from neurodsp.utils.data import split_signal
+
 ###################################################################################################
 ###################################################################################################
 
@@ -39,12 +41,12 @@ def compute_fluctuations(sig, fs, n_scales=10, min_scale=0.01, max_scale=1.0, de
     fluctuations : 1d array
         Average fluctuation at each scale.
     exp : float
-        Slope of line in loglog when plotting t_scales against fluctuations.
+        Slope of line in loglog when plotting time scales against fluctuations.
         This is the alpha value for DFA, or the Hurst exponent for rescaled range.
 
     Notes
     -----
-    These analysis compute fractal properties by analyzing fluctuations across windows.
+    These analyses compute fractal properties by analyzing fluctuations across windows.
 
     Overall, these approaches involve dividing the time-series into non-overlapping
     windows at log-spaced scales and computing a fluctuation measure across windows.
@@ -52,28 +54,25 @@ def compute_fluctuations(sig, fs, n_scales=10, min_scale=0.01, max_scale=1.0, de
     information on the fractal properties of a signal.
 
     Measures available are:
+
     - DFA: detrended fluctuation analysis
         - computes ordinary least squares fits across signal windows
     - RS: rescaled range
         - computes the range of signal windows, divided by the standard deviation
-
-    Empirically, DFA seems to work for a larger range of exponent values.
     """
 
     # Get log10 equi-spaced scales and translate that into window lengths
     t_scales = np.logspace(np.log10(min_scale), np.log10(max_scale), n_scales)
     win_lens = np.round(t_scales * fs).astype('int')
 
-    # Calculate culmulative sum of the signal
-    sig_walk = sp.cumsum(sig - np.mean(sig))
-
-    # Step through each scale and get RMS
+    # Step through each scale and measure fluctuations
     fluctuations = np.zeros_like(t_scales)
     for idx, win_len in enumerate(win_lens):
+
         if method is 'dfa':
-            fluctuations[idx] = compute_detrended_fluctuation(sig_walk, win_len=win_len, deg=deg)
+            fluctuations[idx] = compute_detrended_fluctuation(sig, win_len=win_len, deg=deg)
         elif method is 'rs':
-            fluctuations[idx] = compute_rescaled_range(sig_walk, win_len=win_len)
+            fluctuations[idx] = compute_rescaled_range(sig, win_len=win_len)
         else:
             raise ValueError('Fluctuation method not understood.')
 
@@ -84,14 +83,14 @@ def compute_fluctuations(sig, fs, n_scales=10, min_scale=0.01, max_scale=1.0, de
 
 
 def compute_rescaled_range(sig, win_len):
-    """Compute rescaled range of a given time-series at a given scale.
+    """Compute rescaled range of a given time series at a given scale.
 
     Parameters
     ----------
     sig : 1d array
         Time series.
     win_len : int
-        Window length for each rescaled range computation.
+        Window length for each rescaled range computation, in samples.
 
     Returns
     -------
@@ -99,45 +98,44 @@ def compute_rescaled_range(sig, win_len):
         Average rescaled range over windows.
     """
 
-    # Gather windows & vectorize, so we can call math functions in one go
-    n_win = int(np.floor(len(sig) / win_len))
-    sig_rect = np.reshape(sig[:n_win * win_len], (n_win, win_len)).T
+    # Demean signal
+    sig = sig - np.mean(sig)
 
-    # get back the sig by taking the derivative of sig_walk
-    X = np.concatenate((sig[:1], np.diff(sig)))
-    X_rect = np.reshape(X[:n_win * win_len], (n_win, win_len)).T
+    # Calculate cumulative sum of the signal & split the signal into segments
+    segments = split_signal(sp.cumsum(sig), win_len).T
 
-    # Calculate rescaled range as range divided by std, and take mean across windows
-    rs_win = np.ptp(sig_rect, axis=0) / np.std(X_rect, axis=0)
+    # Calculate rescaled range as range divided by standard deviation (of non-cumulative signal)
+    rs_win = np.ptp(segments, axis=0) / np.std(split_signal(sig, win_len).T, axis=0)
+
+    # Take the mean across windows
     rs = np.mean(rs_win)
 
     return rs
 
 
 def compute_detrended_fluctuation(sig, win_len, deg=1):
-    """Compute detrended fluctuation of a time-series at the given window length.
+    """Compute detrended fluctuation of a time series at the given window length.
 
     Parameters
     ----------
     sig : 1d array
         Time series.
     win_len : int
-        Window length for each detrended fluctuation fit.
+        Window length for each detrended fluctuation fit, in samples.
     deg : int, optional, default=1
         Polynomial degree for detrending.
 
     Returns
     -------
     det_fluc : float
-        Measured detrended fluctuaiton, as the average error fits of the window.
+        Measured detrended fluctuation, as the average error fits of the window.
     """
 
-    # Gather windows & vectorize, so we can call math functions in one go
-    n_win = int(np.floor(len(sig) / win_len))
-    sig_rect = np.reshape(sig[:n_win * win_len], (n_win, win_len)).T
+    # Calculate cumulative sum of the signal & split the signal into segments
+    segments = split_signal(sp.cumsum(sig - np.mean(sig)), win_len).T
 
     # Calculate local trend, as the line of best fit within the time window
-    _, fluc, _, _, _ = np.polyfit(np.arange(win_len), sig_rect, deg=deg, full=True)
+    _, fluc, _, _, _ = np.polyfit(np.arange(win_len), segments, deg=deg, full=True)
 
     # Convert to root-mean squared error, from squared error
     det_fluc = np.mean((fluc / win_len)**0.5)
--- neurodsp/burst/__init__.py
+++ neurodsp/burst/__init__.py
@@ -1,3 +1,4 @@
+"""Burst detection functions."""
 
 from .dualthresh import detect_bursts_dual_threshold
 from .utils import compute_burst_stats
