419c33c9b0d0a5b576bf28d9cd8abdbf04eb6d3c
ryanhammonds <ryan.hammonds@utexas.edu>
2020-08-04 16:40:24 -0700

    faster autocorr

--- neurodsp/aperiodic/autocorr.py
+++ neurodsp/aperiodic/autocorr.py
@@ -5,7 +5,7 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
-def compute_autocorr(sig, max_lag=1000, lag_step=1):
+def compute_autocorr(sig, max_lag=1000, lag_step=1, demean=True):
     """Compute the signal autocorrelation (lagged correlation).
 
     Parameters
@@ -16,6 +16,8 @@ def compute_autocorr(sig, max_lag=1000, lag_step=1):
         Maximum delay to compute autocorrelations for, in samples.
     lag_step : int, optional, default: 1
         Step size (lag advance) for computing autocorrelations.
+    demean : bool, optional, default: True
+        Whether to demean the signal before computing autcorralations.
 
     Returns
     -------
@@ -25,15 +27,13 @@ def compute_autocorr(sig, max_lag=1000, lag_step=1):
         Autocorrelation values, for across time lags.
     """
 
+    if demean:
+        sig = sig - sig.mean()
+
+    autocorrs = np.correlate(sig, sig, "full")[len(sig)-1:]
+    autocorrs = autocorrs[:max_lag] / autocorrs[0]
+    autocorrs = autocorrs[::lag_step]
+
     timepoints = np.arange(0, max_lag, lag_step)
-    autocorrs = np.zeros(len(timepoints))
-
-    autocorrs[0] = np.sum((sig - np.mean(sig))**2)
-
-    for ind, lag in enumerate(timepoints[1:]):
-        autocorrs[ind + 1] = \
-            np.sum((sig[:-lag] - np.mean(sig[:-lag])) * (sig[lag:] - np.mean(sig[lag:])))
-
-    autocorrs = autocorrs / autocorrs[0]
 
     return timepoints, autocorrs
