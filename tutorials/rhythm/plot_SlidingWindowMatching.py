08d8a9b85256538d230cc98956e84a2eb4039f9b
Tom Donoghue <tdonoghue.research@gmail.com>
2020-12-30 16:14:40 -0500

    update & add more text to SWM tut

--- tutorials/rhythm/plot_SlidingWindowMatching.py
+++ tutorials/rhythm/plot_SlidingWindowMatching.py
@@ -2,7 +2,7 @@
 Sliding Window Matching
 =======================
 
-Find recurrent patterns in a neural signal using Sliding Window Matching.
+Find recurring patterns in neural signals using Sliding Window Matching.
 
 This tutorial primarily covers the :func:`~.sliding_window_matching` function.
 """
@@ -11,10 +11,21 @@ This tutorial primarily covers the :func:`~.sliding_window_matching` function.
 # Overview
 # --------
 #
-# This notebook shows how to use sliding window matching (SWM) for identifying recurring
-# patterns in a neural signal, like the shape of an oscillatory waveform.
+# Non-periodic or non-sinusoidal properties can be difficult to assess in frequency domain
+# methods. To try and address this, the sliding window matching (SWM) algorithm has been
+# proposed for detecting and measuring recurring, but unknown, patterns in time series data.
+# Patterns of interest may be transient events, and/or the waveform shape of neural oscillations.
 #
-# For more details on Sliding Window Matching see Gips et al., 2017, J Neuro Methods.
+# In this example, we will explore applying the SWM algorithm to some LFP data.
+#
+# The SWM approach tries to find recurring patterns (or motifs) in the data, using sliding
+# windows. An iterative process samples window randomly, and compares each to the average
+# window. The goal is to find a selection of windows that look maximally like the average
+# window, at which point the occurrences of the window have been detected, and the average
+# window pattern can be examined.
+#
+# The sliding window matching algorithm is described in
+# `Gips et al, 2017 <https://doi.org/10.1016/j.jneumeth.2016.11.001>`_
 #
 
 ###################################################################################################
@@ -39,6 +50,8 @@ set_random_seed(0)
 # Load neural signal
 # ------------------
 #
+# First, we will load a segment of ECoG data, as an example time series.
+#
 
 ###################################################################################################
 
@@ -50,22 +63,57 @@ fs = 1000
 times = create_times(len(sig)/fs, fs)
 
 ###################################################################################################
+#
+# Next, we can visualize this data segment. As we can see this segment of data has
+# some prominent bursts of oscillations, in this case, in the beta frequency.
+#
+
+###################################################################################################
 
 # Plot example signal
 plot_time_series(times, sig)
 
 ###################################################################################################
-# Apply sliding window matching to neural signal
-# ----------------------------------------------
+# Apply sliding window matching
+# -----------------------------
 #
-# The loaded neural signal has a beta oscillation, that we can attempt to analyze
-# with the sliding window matching approach.
-#
-# We will define the window length to be about 1 cycle, which should roughly extract
-# the waveform shape of the neural oscillation.
+# The beta oscillation in our data segment looks like it might have some non-sinusoidal
+# properties. We can investigate this with sliding window matching.
 #
 # Sliding window matching can be applied with the
 # :func:`~.sliding_window_matching` function.
+
+###################################################################################################
+# Data Preprocessing
+# ~~~~~~~~~~~~~~~~~~
+#
+# Typically, the input signal does not have to be filtered into a band of interest to use SWM.
+#
+# If the goal is to characterize non-sinusoidal rhythms, you typically won't want to
+# apply a filter that will smooth out the features of interest.
+#
+# However, if the goal is to characterize higher frequency activity, it can be useful to
+# apply a highpass filter, so that the method does not converge on a lower frequency motif.
+#
+# In our case, the beta rhythm of interest is the most prominent, low frequency, feature of the
+# data, so we won't apply a filter.
+#
+
+###################################################################################################
+# Algorithm Settings
+# ~~~~~~~~~~~~~~~~~~
+#
+# The SWM algorithm has some algorithm specific settings that need to be applied, including:
+#
+# - `win_len` : the length of the window, defined in seconds
+# - `win_spacing` : the minimum distance between windows, also defined in seconds
+#
+# The length of the window influences the patterns that are extracted from the data.
+# Typically, you want to set the window length to match the expected timescale of the
+# patterns under study.
+#
+# For our purposes, we will define the window length to be about 1 cycle of a beta oscillation,
+# which should help the algorithm to find the waveform shape of the neural oscillation.
 #
 
 ###################################################################################################
@@ -74,13 +122,21 @@ plot_time_series(times, sig)
 win_len = .055
 win_spacing = .2
 
+###################################################################################################
+
 # Apply the sliding window matching algorithm to the time series
-avg_window, window_starts, costs = sliding_window_matching(sig, fs, win_len, win_spacing,
-                                                           max_iterations=500)
+avg_window, window_starts, costs = sliding_window_matching(sig, fs, win_len, win_spacing)
 
 ###################################################################################################
+# Examine the Results
+# ~~~~~~~~~~~~~~~~~~~
 #
-# You can plot the resulting pattern with :func:`~.plot_swm_pattern`.
+# What we got back from the SWM function are the calculate average window, the list
+# of indices in the data of the windows, and the calculated costs for each iteration of
+# the algorithm run.
+#
+# In order to visualize the resulting pattern, we can use
+# :func:`~.plot_swm_pattern`.
 #
 
 ###################################################################################################
@@ -90,10 +146,21 @@ plot_swm_pattern(avg_window)
 
 ###################################################################################################
 #
-# Notice that the beta cycles have sharper troughs than peaks, and the average window is
-# a beta cycle with a sharp trough.
+# In the above average pattern, that looks to capture a beta rhythm, we can notice some
+# waveform shape of the extracted rhythm.
 #
-# One thing to explore is how these results change by changing the random seed.
+
+###################################################################################################
+# Concluding Notes
+# ~~~~~~~~~~~~~~~~
 #
-# Using more data and increasing the number of iterations helps the robustness of the algorithm.
+# One thing to keep in mind is that the SWM algorithm includes a random element of sampling
+# and comparing the windows - meaning it is not deterministic. Because of this, results
+# can change with different random seeds.
+#
+# To explore this, go back and change the random seed, and see how the output changes.
+#
+# You can also set the number of iterations that the algorithm sweeps through. Increasing
+# the number of iterations, and using longer data segments, can help improve the robustness
+# of the algorithm results.
 #
