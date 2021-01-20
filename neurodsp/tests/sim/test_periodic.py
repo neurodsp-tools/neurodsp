1c84d94901e8a5ea369ba2e3b671d1b87c6748ef
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-20 00:19:12 -0500

    fix tests

--- neurodsp/tests/sim/test_periodic.py
+++ neurodsp/tests/sim/test_periodic.py
@@ -21,12 +21,12 @@ def test_sim_bursty_oscillation():
 
     # Check probability burst approach
     sig2 = sim_bursty_oscillation(N_SECONDS, FS, FREQ1, \
-        burst_approach='prob', burst_params={'enter_burst' : 0.3, 'leave_burst' : 0.3})
+        burst_def='prob', burst_params={'enter_burst' : 0.3, 'leave_burst' : 0.3})
     check_sim_output(sig2)
 
     # Check durations burst approach
     sig3 = sim_bursty_oscillation(N_SECONDS, FS, FREQ1, \
-        burst_approach='durations', burst_params={'n_cycles_burst' : 2, 'n_cycles_off' : 2})
+        burst_def='durations', burst_params={'n_cycles_burst' : 2, 'n_cycles_off' : 2})
     check_sim_output(sig3)
 
 def test_make_bursts():
