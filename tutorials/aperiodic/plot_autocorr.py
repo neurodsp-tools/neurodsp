006fe2bf9b8956539d9f06b16eacaa7a396f24db
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-20 19:46:00 -0500

    update ac tut

--- tutorials/aperiodic/plot_autocorr.py
+++ tutorials/aperiodic/plot_autocorr.py
@@ -4,26 +4,65 @@ Autocorrelation Measures
 
 Apply autocorrelation measures to neural signals.
 
+Autocorrelation is the correlation of a signal with delayed copies of itself. Autocorrelation
+measures can be a useful measure to investigate properties of neural signals.
+
+In this example, we explore computing autocorrelation for simulated neural signals.
+
 This tutorial covers ``neurodsp.aperiodic.autocorr``.
 """
 
 ###################################################################################################
 
+# sphinx_gallery_thumbnail_number = 1
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from neurodsp.sim import sim_powerlaw, sim_oscillation
+
+# Import the function for computing autocorrelation
 from neurodsp.aperiodic import compute_autocorr
 
 ###################################################################################################
+# Autocorrelation Measures
+# ------------------------
+#
+# Autocorrelation is computed as the correlation between the original signal and delayed
+# copies, across different lags.
+#
+# The result is a measure of how correlated a signal is to itself, across time,
+# and the timescale of autocorrelation.
+#
+#
+# Algorithm Settings
+# ~~~~~~~~~~~~~~~~~~
+#
+# Settings for computing autocorrelation are:
+#
+# - `max_lag` : the maximum lag to compute autocorrelation for
+# - `lag_step` : the step size to advance across when computing autocorrelation
+#
+# Both parameters are defined in samples, with defaults of a step of 1 sample, up to
+# a max lag of 1000 samples.
+#
+# Autocorrelation can be computed with :func:`~.compute_autocorr` function, which
+# returns `timepoints` at which autocorrelation was calculated, and `autocorrs`,
+# the resulting correlation coefficients.
+#
 
 ###################################################################################################
-# Simulate Data
-# -------------
+# Autocorrelation of Periodic Signals
+# -----------------------------------
 #
-# A sine wave will be used for the first example to highlight how autocorrelations map to the
-# phase of an oscillatory signal. A second signal, containing white noise, will be used to
-# highlight the absence of an autocorrelation structure.
+# First, let's examine periodic signals, specifically sine waves.
+#
+# Note that periodic signals are by definition rhythmic. This means that we should expect
+# the autocorrelation to also have a rhythmic pattern across time.
+#
+# Imagine, for example, moving one sine wave across another. At some points, when they are
+# in phase, they will line up exactly (have a high correlation), while at others, when they
+# are out of phase, they will have either low or anti-correlation.
 #
 
 ###################################################################################################
@@ -31,110 +70,101 @@ from neurodsp.aperiodic import compute_autocorr
 # Simulation settings
 n_seconds = 10
 fs = 1000
-freq = 10
-freq_samp = int(fs / freq)
 
-# Simulate signals
-sig_osc = sim_oscillation(n_seconds, fs, freq)
-sig_wn = sim_powerlaw(n_seconds, fs, exponent=0)
+# Define the frequency of the sinusoid
+freq1 = 10
+freq2 = 20
 
-###################################################################################################
-# Compute Autocorrelations
-# ------------------------
-#
-# The autocorrelation will be computed between the original signal and sliced signals using various
-# lag lengths. The lagged signals are shifted in steps of 1 from 0 to 1000 samples (default). The
-# :func:`~.compute_autocorr` function returns ``timepoints``, the original signal sample indices
-# where each lagged signal begins, and ``autocorrs``, the correlation coefficient between the
-# original signal and each lagged signal.
-#
-
-# Compute the autocorrelations using various lag lengths
-timepoints_osc, autocorrs_osc = compute_autocorr(sig_osc)
-timepoints_wn, autocorrs_wn = compute_autocorr(sig_wn)
+# Simulate a sinusoid
+sig_osc1 = sim_oscillation(n_seconds, fs, freq1)
+sig_osc2 = sim_oscillation(n_seconds, fs, freq2)
 
 ###################################################################################################
+
+# Compute autocorrelation on the periodic time series
+timepoints_osc1, autocorrs_osc1 = compute_autocorr(sig_osc1)
+timepoints_osc2, autocorrs_osc2 = compute_autocorr(sig_osc2)
+
+###################################################################################################
+# Autocorrelation of a sinusoidal signal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Result: Sine Wave
-# -----------------
-#
-# The autocorrlations of the sine wave simulation are plotted below. The maximum, minimum,
-# decay zero-crossings, rise zero-crossings of the autocorrelation plot correspond to a phase shift
-# of 2pi, pi, pi/2, and 3pi/2, respectively.
+# The autocorrelation of the first sine wave is plotted below.
 #
 
-# Define max, min, and zero-crossing autocorrlation locations
-max_lag = 1000
-phase_2_pi = np.arange(0, max_lag, freq_samp, dtype=int)
-phase_pi = np.arange(0, max_lag, freq_samp/2, dtype=int)[1::2]
-phase_pi_2 = np.arange(0, max_lag, freq_samp/4, dtype=int)[1::4]
-phase_3pi_2 = np.arange(0, max_lag, freq_samp/4, dtype=int)[3::4]
+###################################################################################################
 
 # Plot autocorrelations
-figsize=(10, 8)
-fig, ax = plt.subplots(figsize=figsize)
-
-ax.plot(timepoints_osc, autocorrs_osc, label="Auto-correlations")
-
-ax.plot(phase_2_pi, autocorrs_osc[phase_2_pi],
-        ls="", marker="o", ms=6, label="Phase shift: 2pi")
-
-ax.plot(phase_pi, autocorrs_osc[phase_pi],
-        ls="", marker="o", ms=6, label="Phase shift: pi")
-
-ax.plot(phase_pi_2, autocorrs_osc[phase_pi_2],
-        ls="", marker="o", ms=6, label="Phase shift: pi/2")
-
-ax.plot(phase_3pi_2, autocorrs_osc[phase_3pi_2],
-        ls="", marker="o", ms=6, label="Phase shift: 3pi/2")
-
-ax.set_ylabel("Auto-Correlation Coefficient")
-ax.set_xlabel("Lag Length (samples)")
-ax.set_title("Auto-Correlation: Sine Wave")
-ax.legend(loc="upper right")
-
-###################################################################################################
-
-# Plot phase shifts
-fig, ax = plt.subplots(figsize=figsize)
-
-cyc_times = np.arange(0, freq_samp)
-
-# Reference cycle
-ax.plot(cyc_times, sig_osc[:freq_samp], label="Reference")
-
-# Phase shift: 2pi
-ax.plot(cyc_times, sig_osc[freq_samp:2*freq_samp],
-        ls="-", dashes=(4, 4), label="Phase: 2pi")
-
-# Phase shift: pi
-ax.plot(cyc_times, sig_osc[int(freq_samp/2):freq_samp+int(freq_samp/2):],
-        ls="-", dashes=(4, 4), label="Phase: pi")
-
-# Phase shift: pi/2
-ax.plot(cyc_times, sig_osc[int(freq_samp/4):freq_samp+int(freq_samp/4):],
-        ls="-", dashes=(4, 4), label="Phase: pi/2")
-
-#Phase shift: 3pi/2
-ax.plot(cyc_times, sig_osc[int(3*freq_samp/4):freq_samp+int(3*freq_samp/4):],
-        ls="-", dashes=(4, 4), label="Phase: 3pi/2")
-
-ax.set_xlabel("Samples")
-ax.set_ylabel("Signal")
-ax.set_title("Phase Shifts")
-ax.legend(loc="upper right")
+_, ax = plt.subplots(figsize=(6, 4))
+ax.plot(timepoints_osc1, autocorrs_osc1)
+ax.set(xlabel='lag (samples)', ylabel='autocorrelation');
 
 ###################################################################################################
 #
-# Result: White Noise
-# -------------------
+# As we can see, the autocorrelation of a sinusoid it itself a sinusoid!
 #
-# The autocorrelation of a signal containing white noise will have a single peak (dirac) at
-# ``timepoints == 0``. The autocorrelation coefficient at all other timepoints will be near zero.
+# This reflects that a sinusoid related to itself will oscillate between between
+# positively and negatively correlated with itself.
+#
+# Next, let's compare the autocorrelation of different sinusoids.
 #
 
-fig, ax = plt.subplots(figsize=figsize)
-ax.plot(timepoints_wn, autocorrs_wn)
-ax.set_ylabel("Auto-Correlation Coefficient")
-ax.set_xlabel("Lag Length (samples)")
-ax.set_title("Auto-Correlation: White Noise")
+###################################################################################################
+
+_, ax = plt.subplots(figsize=(6, 4))
+ax.plot(timepoints_osc1, autocorrs_osc1, alpha=0.75, label='10 Hz')
+ax.plot(timepoints_osc2, autocorrs_osc2, alpha=0.75, label='20 Hz')
+ax.set(xlabel='lag (samples)', ylabel='autocorrelation')
+plt.legend(loc='upper right')
+
+###################################################################################################
+#
+# In the above, we can see that the autocorrelation of sinusoids with different frequencies
+# leads to autocorrelation results with different timescales.
+#
+# If you compare to the number of samples on the x-axis, keeping in mind a sampling
+# rate (of 1000 Hz, here), you can check that the autocorrelation of a sinusoidal
+# signal is itself a sinusoid of the same frequency.
+#
+
+###################################################################################################
+# Autocorrelation of Aperiodic Signals
+# ------------------------------------
+#
+# Next, lets consider the autocorrelation of aperiodic signals.
+#
+# Here we will use white noise, as an example of a signal without autocorrelation, and
+# pink noise, which does, by definition, have temporal auto-correlations.
+#
+
+###################################################################################################
+
+# Simulate a white noise signal
+sig_wn = sim_powerlaw(n_seconds, fs, exponent=0)
+
+# Simulate a pink noise signal
+sig_pn = sim_powerlaw(n_seconds, fs, exponent=-1)
+
+###################################################################################################
+
+# Compute autocorrelation on the aperiodic time series
+timepoints_wn, autocorrs_wn = compute_autocorr(sig_wn)
+timepoints_pn, autocorrs_pn = compute_autocorr(sig_pn)
+
+###################################################################################################
+
+# Plot the autocorrelations of the aperiodic signals
+_, ax = plt.subplots(figsize=(5, 4))
+ax.plot(timepoints_wn, autocorrs_wn, label='White Noise')
+ax.plot(timepoints_pn, autocorrs_pn, label='Pink Noise')
+ax.set(xlabel="lag (samples)", ylabel="autocorrelation")
+plt.legend()
+
+###################################################################################################
+#
+# In the above, we can see that for white noise, the autocorrelation is only high at
+# a lag of 0 samples, with all other lags being uncorrelated.
+#
+# By comparison, the pink noise signal has a pattern of decreasing autocorrelation
+# across increasing lags. This is characteristic of powerlaw data.
+#
