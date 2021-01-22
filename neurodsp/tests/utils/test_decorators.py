4e0985eb41fd7bf4a919da07fd9c2baa1e425e25
Tom Donoghue <tdonoghue.research@gmail.com>
2021-01-21 23:19:08 -0500

    add tests for decorators

--- neurodsp/tests/utils/test_decorators.py
+++ neurodsp/tests/utils/test_decorators.py
@@ -1,12 +1,29 @@
 """Tests for neurodsp.utils.decorators."""
 
+import numpy as np
+
 from neurodsp.utils.decorators import *
 
 ###################################################################################################
 ###################################################################################################
 
 def test_normalize():
-    pass
+
+    @normalize
+    def func():
+        return np.array([1, 2, 3, 4, 5])
+
+    # Check that output of function gets normalized
+    out = func()
+    assert np.isclose(out.mean(), 0.)
+    assert np.isclose(out.var(), 1.)
 
 def test_multidim():
-    pass
+
+    @multidim(select=[])
+    def func(sig):
+        return np.sum(sig)
+
+    # Check that the output of the func gets applied across dimensions
+    out = func(np.array([[1, 2], [1, 2]]))
+    assert np.array_equal(out, np.array([3, 3]))
