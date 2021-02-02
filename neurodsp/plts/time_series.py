f4999d0ee32742d680cc09b4898c13a4aaa8b9bf
ryanhammonds <ryan.hammonds@utexas.edu>
2021-02-02 12:23:37 -0800

    allow plotting of 2d arrays

--- neurodsp/plts/time_series.py
+++ neurodsp/plts/time_series.py
@@ -19,9 +19,9 @@ def plot_time_series(times, sigs, labels=None, colors=None, ax=None, **kwargs):
 
     Parameters
     ----------
-    times : 1d array or list of 1d array
+    times : 1d array, list of 1d array, or 2d array
         Time definition(s) for the time series to be plotted.
-    sigs : 1d array or list of 1d array
+    sigs : 1d array, list of 1d array, or 2d array
         Time series to plot.
     labels : list of str, optional
         Labels for each time series.
@@ -47,18 +47,24 @@ def plot_time_series(times, sigs, labels=None, colors=None, ax=None, **kwargs):
 
     ax = check_ax(ax, (15, 3))
 
-    times = repeat(times) if isinstance(times, np.ndarray) else times
-    sigs = [sigs] if isinstance(sigs, np.ndarray) else sigs
+    n_repeats = len(sigs) if isinstance(sigs, list) or sigs.ndim == 2 else 1
+
+    # Repeat times if needed
+    if isinstance(times, np.ndarray) and times.ndim != 2:
+        times = np.tile(times, (n_repeats, 1))
+
+    # Make sigs iterable if 1D
+    sigs = np.reshape(sigs, (1, -1)) if not isinstance(sigs, list) and sigs.ndim == 1 else sigs
 
     if labels is not None:
         labels = [labels] if not isinstance(labels, list) else labels
     else:
-        labels = repeat(labels)
+        labels = repeat(labels, n_repeats)
 
     # If not provided, default colors for up to two signals to be black & red
     if not colors and len(sigs) <= 2:
         colors = ['k', 'r']
-    colors = repeat(colors) if not isinstance(colors, list) else cycle(colors)
+    colors = repeat(colors, n_repeats) if not isinstance(colors, list) else cycle(colors)
 
     for time, sig, color, label in zip(times, sigs, colors, labels):
         ax.plot(time, sig, color=color, label=label)
