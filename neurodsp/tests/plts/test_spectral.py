d346569f0f60c6fed5cb2bc7d4ee1ac2890e991d
Tom Donoghue <tdonoghue.research@gmail.com>
2021-03-14 15:57:14 -0400

    make test plots more realistic

--- neurodsp/tests/plts/test_spectral.py
+++ neurodsp/tests/plts/test_spectral.py
@@ -2,10 +2,10 @@
 
 import numpy as np
 
-from neurodsp.spectral.variance import compute_spectral_hist
 from neurodsp.spectral.power import compute_spectrum
+from neurodsp.spectral.variance import compute_spectral_hist, compute_scv, compute_scv_rs
 
-from neurodsp.tests.settings import TEST_PLOTS_PATH
+from neurodsp.tests.settings import TEST_PLOTS_PATH, FS
 from neurodsp.tests.tutils import plot_test
 
 from neurodsp.plts.spectral import *
@@ -14,55 +14,55 @@ from neurodsp.plts.spectral import *
 ###################################################################################################
 
 @plot_test
-def test_plot_power_spectra():
+def test_plot_power_spectra(tsig_comb, tsig_burst):
 
-    freqs = np.array([1, 2, 3, 4])
-    powers = np.array([10, 20, 30, 40])
+    freqs1, powers1 = compute_spectrum(tsig_comb, FS)
+    freqs2, powers2 = compute_spectrum(tsig_burst, FS)
 
-    plot_power_spectra(freqs, powers,
+    plot_power_spectra(freqs1, powers1,
                        save_fig=True, file_path=TEST_PLOTS_PATH,
                        file_name='test_plot_power_spectra-1.png')
 
-    plot_power_spectra([freqs, freqs], [powers, powers],
-                       labels=['a', 'b'], colors=['k', 'r'],
+    plot_power_spectra([freqs1, freqs2], [powers1, powers2],
+                       labels=['first', 'second'], colors=['k', 'r'],
                        save_fig=True, file_path=TEST_PLOTS_PATH,
                        file_name='test_plot_power_spectra-2.png')
 
 @plot_test
-def test_plot_scv():
+def test_plot_scv(tsig_comb):
 
-    freqs, scv = np.array([1, 2, 3, 4]), np.array([10, 20, 30, 40])
+    freqs, scv = compute_scv(tsig_comb, FS)
 
     plot_scv(freqs, scv,
              save_fig=True, file_path=TEST_PLOTS_PATH,
              file_name='test_plot_scv.png')
 
-@plot_test
-def test_plot_scv_rs_lines():
+def test_other_scv_plots(tsig_comb):
 
-    freqs = np.array([1, 2, 3])
-    scv_rs = np.array([[2, 3, 4], [2, 3, 4], [3, 4, 5]])
+    freqs, t_inds, scv_rs = compute_scv_rs(tsig_comb, FS, nperseg=FS/2, method='rolling')
+
+    _plot_scv_rs_lines(freqs, scv_rs)
+    _plot_scv_rs_matrix(freqs, t_inds, scv_rs)
+
+@plot_test
+def _plot_scv_rs_lines(freqs, scv_rs):
 
     plot_scv_rs_lines(freqs, scv_rs,
                       save_fig=True, file_path=TEST_PLOTS_PATH,
                       file_name='test_plot_scv_rs_lines.png')
 
 @plot_test
-def test_plot_scv_rs_matrix():
+def _plot_scv_rs_matrix(freqs, t_inds, scv_rs):
 
-    freqs = np.array([1, 2, 3])
-    times = np.array([1, 2, 3])
-    scv_rs = np.array([[2, 3, 4], [2, 3, 4], [3, 4, 5]])
-
-    plot_scv_rs_matrix(freqs, times, scv_rs,
+    plot_scv_rs_matrix(freqs, t_inds, scv_rs,
                        save_fig=True, file_path=TEST_PLOTS_PATH,
                        file_name='test_plot_scv_rs_matrix.png')
 
 @plot_test
-def test_plot_spectral_hist(tsig):
+def test_plot_spectral_hist(tsig_comb):
 
-    freqs, power_bins, spectral_hist = compute_spectral_hist(tsig, fs=1000)
-    spectrum_freqs, spectrum = compute_spectrum(tsig, fs=1000)
+    freqs, power_bins, spectral_hist = compute_spectral_hist(tsig_comb, fs=FS)
+    spectrum_freqs, spectrum = compute_spectrum(tsig_comb, fs=FS)
 
     plot_spectral_hist(freqs, power_bins, spectral_hist,
                        spectrum=spectrum, spectrum_freqs=spectrum_freqs,
--- neurodsp/tests/plts/test_time_series.py
+++ neurodsp/tests/plts/test_time_series.py
@@ -7,7 +7,7 @@ from neurodsp.utils import create_times
 from neurodsp.burst import detect_bursts_dual_threshold
 from neurodsp.timefrequency import amp_by_time, phase_by_time, freq_by_time
 
-from neurodsp.tests.settings import TEST_PLOTS_PATH, N_SECONDS, N_SECONDS_LONG, FS_HIGH, F_RANGE
+from neurodsp.tests.settings import TEST_PLOTS_PATH, N_SECONDS, FS, F_RANGE
 from neurodsp.tests.tutils import plot_test
 
 from neurodsp.plts.time_series import *
@@ -16,52 +16,52 @@ from neurodsp.plts.time_series import *
 ###################################################################################################
 
 @plot_test
-def test_plot_time_series(tsig, tsig2d):
+def test_plot_time_series(tsig_comb, tsig_burst):
 
-    times = create_times(N_SECONDS, FS_HIGH)
+    times = create_times(N_SECONDS, FS)
 
     # Check single time series plot
-    plot_time_series(times, tsig,
+    plot_time_series(times, tsig_comb,
                      save_fig=True, file_path=TEST_PLOTS_PATH,
                      file_name='test_plot_time_series-1.png')
 
     # Check multi time series plot, labels
-    plot_time_series(times, [tsig, tsig[::-1]],
+    plot_time_series(times, [tsig_comb, tsig_comb[::-1]],
                      labels=['signal', 'signal reversed'],
                      save_fig=True, file_path=TEST_PLOTS_PATH,
                      file_name='test_plot_time_series-2.png')
 
     # Test 2D arrays
-    plot_time_series(times, tsig2d,
+    plot_time_series(times, np.array([tsig_comb, tsig_burst]),
                      save_fig=True, file_path=TEST_PLOTS_PATH,
                      file_name='test_plot_time_series-2arr.png')
 
 @plot_test
-def test_plot_instantaneous_measure(tsig):
+def test_plot_instantaneous_measure(tsig_comb):
 
-    times = create_times(N_SECONDS, FS_HIGH)
+    times = create_times(N_SECONDS, FS)
 
-    plot_instantaneous_measure(times, amp_by_time(tsig, FS_HIGH, F_RANGE), 'amplitude',
+    plot_instantaneous_measure(times, amp_by_time(tsig_comb, FS, F_RANGE), 'amplitude',
                                save_fig=True, file_path=TEST_PLOTS_PATH,
                                file_name='test_plot_instantaneous_measure_amplitude.png')
 
-    plot_instantaneous_measure(times, phase_by_time(tsig, FS_HIGH, F_RANGE), 'phase',
+    plot_instantaneous_measure(times, phase_by_time(tsig_comb, FS, F_RANGE), 'phase',
                                save_fig=True, file_path=TEST_PLOTS_PATH,
                                file_name='test_plot_instantaneous_measure_phase.png')
 
-    plot_instantaneous_measure(times, freq_by_time(tsig, FS_HIGH, F_RANGE), 'frequency',
+    plot_instantaneous_measure(times, freq_by_time(tsig_comb, FS, F_RANGE), 'frequency',
                                save_fig=True, file_path=TEST_PLOTS_PATH,
                                file_name='test_plot_instantaneous_measure_frequency.png')
 
     # Check the error for bad measure
     with raises(ValueError):
-        plot_instantaneous_measure(times, tsig, 'BAD')
+        plot_instantaneous_measure(times, tsig_comb, 'BAD')
 
 @plot_test
 def test_plot_bursts(tsig_burst):
 
-    times = create_times(N_SECONDS_LONG, FS_HIGH)
-    bursts = detect_bursts_dual_threshold(tsig_burst, FS_HIGH, (0.75, 1.5), F_RANGE)
+    times = create_times(N_SECONDS, FS)
+    bursts = detect_bursts_dual_threshold(tsig_burst, FS, (0.75, 1.5), F_RANGE)
 
     plot_bursts(times, tsig_burst, bursts,
                 save_fig=True, file_path=TEST_PLOTS_PATH,
