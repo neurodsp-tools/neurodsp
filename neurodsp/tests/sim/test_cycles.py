ddf82e117a73e18f5a8f41df8a804aff0331891f
ryanhammonds <ryan.hammonds@utexas.edu>
2021-07-30 14:14:02 -0700

    updated sim cycles tests

--- neurodsp/tests/sim/test_cycles.py
+++ neurodsp/tests/sim/test_cycles.py
@@ -108,13 +108,13 @@ def test_sim_skewed_gaussian_cycle():
 
 def test_sim_gaussian_asym_cycle():
 
-    cycle = sim_asym_beta_cycle(N_SECONDS_CYCLE, FS, 0.5)
+    cycle = sim_exp_cos_cycle(N_SECONDS_CYCLE, FS, 0.5)
     check_sim_output(cycle, n_seconds=N_SECONDS_CYCLE)
 
-    cycle = sim_asym_beta_cycle(N_SECONDS_ODD, FS, 0.5)
+    cycle = sim_exp_cos_cycle(N_SECONDS_ODD, FS, 0.5)
     check_sim_output(cycle, n_seconds=N_SECONDS_ODD)
 
-    cycle = sim_asym_beta_cycle(N_SECONDS_CYCLE, FS_ODD, 0.5)
+    cycle = sim_exp_cos_cycle(N_SECONDS_CYCLE, FS_ODD, 0.5)
     check_sim_output(cycle, n_seconds=N_SECONDS_CYCLE, fs=FS_ODD)
 
 def test_sim_harmonic_asym_cycle():
