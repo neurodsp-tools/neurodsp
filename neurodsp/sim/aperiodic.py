dc33c3d99e0bcc1d10826fdb2351c187a0492101
Tom Donoghue <tdonoghue.research@gmail.com>
2021-10-14 01:16:51 -0400

    lints

--- neurodsp/sim/aperiodic.py
+++ neurodsp/sim/aperiodic.py
@@ -119,47 +119,47 @@ def sim_synaptic_current(n_seconds, fs, n_neurons=1000, firing_rate=2.,
 
 @normalize
 def sim_knee(n_seconds, fs, chi1, chi2, knee):
-    """Returns a time series whose power spectrum follows the Lorentzian equation:
-
-    P(f) = 1 / (f**chi1 * (f**chi2 + knee))
-
-    using a sum of sinusoids.
+    """Simulate a signal whose power spectrum has a 1/f structure with a knee.
 
     Parameters
-    -----------
+    ----------
     n_seconds: float
-        Number of seconds elapsed in the time series.
+        Simulation time, in seconds.
     fs: float
-        Sampling rate.
+        Sampling rate of simulated signal, in Hz.
     chi1: float
         Power law exponent before the knee.
     chi2: float
         Power law exponent added to chi1 after the knee.
     knee: float
-        Location of the knee in hz.
+        Location of the knee in Hz.
 
     Returns
     -------
-    sig: 1d array
-        Time series with desired power spectrum.
+    sig : 1d array
+        Time series with the desired power spectrum.
 
     Notes
     -----
-    The slope of the log power spectrum before the knee is chi1 whereas after the knee it is chi2,
+    This simulated time series has a power spectrum that follows the Lorentzian equation:
+
+    `P(f) = 1 / (f**chi1 * (f**chi2 + knee))`
+
+    - This simulation creates this power spectrum shape using a sum of sinusoids.
+    - The slope of the log power spectrum before the knee is chi1 whereas after the knee it is chi2,
     but only when the sign of chi1 and chi2 are the same.
 
     Examples
     --------
-    Simulate a time series with (chi1, chi2, knee) = (-1, -2, 100)
+    Simulate a time series with chi1 of -1, chi2 of -2, and knee of 100:
 
-    >> sim_knee(n_seconds=10, fs=10**3, chi1=-1, chi2=-2, knee=100)
+    >> sim_knee(n_seconds=10, fs=1000, chi1=-1, chi2=-2, knee=100)
     """
 
     times = create_times(n_seconds, fs)
     n_samples = compute_nsamples(n_seconds, fs)
 
-    # Create the range of frequencies that appear in the power spectrum since these
-    #   will be the frequencies in the cosines we sum below
+    # Create frequencies for the power spectrum, which will be freqs of the summed cosines
     freqs = np.linspace(0, fs/2, num=int(n_samples//2 + 1), endpoint=True)
 
     # Drop the DC component
@@ -167,16 +167,13 @@ def sim_knee(n_seconds, fs, chi1, chi2, knee):
 
     # Map the frequencies under the (square root) Lorentzian
     #   This will give us the amplitude coefficients for the sinusoids
-    cosine_coeffs = np.array([np.sqrt(1/(f**-chi1*(f**(-chi2-chi1) + knee))) for f in freqs])
+    cosine_coeffs = np.array([np.sqrt(1 / (freq ** -chi1 * (freq ** (-chi2 - chi1) + knee))) \
+        for freq in freqs])
 
     # Add sinusoids with a random phase shift
-    sig = np.sum(
-        np.array(
-            [cosine_coeffs[ell]*np.cos(2*np.pi*f*times + 2*np.pi*np.random.rand())
-             for ell, f in enumerate(freqs)]
-        ),
-        axis=0
-    )
+    sig = np.sum(np.array([cosine_coeffs[ell] * \
+                          np.cos(2 * np.pi * freq * times + 2 * np.pi * np.random.rand()) \
+                 for ell, freq in enumerate(freqs)]), axis=0)
 
     return sig
 
@@ -200,7 +197,7 @@ def sim_random_walk(n_seconds, fs, theta=1., mu=0., sigma=5.):
 
     Returns
     -------
-    sig: 1d array
+    sig : 1d array
         Simulated random walk signal.
 
     Notes
@@ -262,7 +259,7 @@ def sim_powerlaw(n_seconds, fs, exponent=-2.0, f_range=None, **filter_kwargs):
 
     Returns
     -------
-    sig: 1d array
+    sig : 1d array
         Time-series with the desired power law exponent.
 
     Examples
@@ -318,7 +315,7 @@ def _create_powerlaw(n_samples, fs, exponent):
 
     Returns
     -------
-    sig: 1d array
+    sig : 1d array
         Time-series with the desired power law exponent.
 
     Notes
