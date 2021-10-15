383a65d8c912d471a5cabe8a38f0c55496546ed9
ryanhammonds <ryan.hammonds@utexas.edu>
2021-10-15 12:39:11 -0700

    update inits

--- neurodsp/filt/__init__.py
+++ neurodsp/filt/__init__.py
@@ -3,4 +3,6 @@
 from .filter import filter_signal
 from .fir import filter_signal_fir, design_fir_filter
 from .iir import filter_signal_iir, design_iir_filter
-from .utils import infer_passtype
+from .utils import (infer_passtype, compute_frequency_response, compute_pass_band,
+    compute_transition_band, compute_nyquist, remove_filter_edges)
+from .checks import check_filter_definition, check_filter_properties
