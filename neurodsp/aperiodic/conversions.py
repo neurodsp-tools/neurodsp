5e9469d949d59fa2d92cba62068a2eb1f9d736ae
Tom Donoghue <tdonoghue.research@gmail.com>
2022-01-17 22:39:07 -0500

    add exp -> hfd

--- neurodsp/aperiodic/conversions.py
+++ neurodsp/aperiodic/conversions.py
@@ -1,9 +1,9 @@
-"""Convert between measures."""
+"""Convert between aperiodic and related measures."""
 
 ###################################################################################################
 ###################################################################################################
 
-def convert_exponent_alpha(exp):
+def convert_exponent_alpha(exponent):
     """Convert a powerlaw exponent to the expected DFA alpha value.
 
     Parameters
@@ -13,11 +13,12 @@ def convert_exponent_alpha(exp):
 
     Returns
     -------
-    float
+    alpha : float
         Predicted alpha value for the given exponent value.
     """
 
-    return (-exp + 1) / 2
+    return (-exponent + 1) / 2
+
 
 def convert_alpha_exponent(alpha):
     """Convert a DFA alpha value to the expected powerlaw exponent.
@@ -29,8 +30,42 @@ def convert_alpha_exponent(alpha):
 
     Returns
     -------
-    float
+    exponent : float
         Predicted exponent value for the given alpha value.
     """
 
     return -2 * alpha + 1
+
+
+def convert_exponent_hfd(exponent):
+    """Convert exponent to expected Higuchi fractal dimension value.
+
+    Parameters
+    ----------
+    exponent : float
+        Exponent value.
+
+    Returns
+    -------
+    hfd : float
+        Predicted Higuchi fractal dimension value.
+
+    Notes
+    -----
+    This works for exponents between {1, 3} (inclusive).
+    As a special case, if exp is 0, D is 2.
+
+    References
+    ----------
+    From F Cervantes-De la Torre et al, 2013
+    """
+
+    if exponent == 0:
+        hfd = 2
+    elif exponent >= 1 and exponent <= 3:
+        hfd = (5 - exponent) / 2
+    else:
+        msg = 'Conversion not supported for given exponent value.'
+        raise ValueError(msg)
+
+    return hfd
