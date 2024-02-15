cf503c96cd1ae82b7f7262ff4c6fff35b618e48d
Tom Donoghue <tdonoghue.research@gmail.com>
2024-02-15 17:16:59 -0500

    add new plots to init

--- neurodsp/plts/__init__.py
+++ neurodsp/plts/__init__.py
@@ -1,8 +1,10 @@
 """Plotting functions."""
 
-from .time_series import plot_time_series, plot_bursts, plot_instantaneous_measure
+from .time_series import (plot_time_series, plot_bursts, plot_instantaneous_measure
+                          plot_multi_time_series)
 from .filt import plot_filter_properties, plot_frequency_response, plot_impulse_response
 from .rhythm import plot_swm_pattern, plot_lagged_coherence
 from .spectral import (plot_power_spectra, plot_spectral_hist,
                        plot_scv, plot_scv_rs_lines, plot_scv_rs_matrix)
 from .timefrequency import plot_timefrequency
+from .combined import plot_timeseries_and_spectrum
