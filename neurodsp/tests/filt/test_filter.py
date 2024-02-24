1f2b6483e2f7fddc89629ac36b26a8b97a0400e1
Tom Donoghue <tdonoghue.research@gmail.com>
2024-02-23 22:33:35 -0500

    add tests for filter input checking

--- neurodsp/tests/filt/test_filter.py
+++ neurodsp/tests/filt/test_filter.py
@@ -7,6 +7,7 @@ import numpy as np
 from neurodsp.tests.settings import FS, F_RANGE
 
 from neurodsp.filt.filter import *
+from neurodsp.filt.filter import _filter_input_checks
 
 ###################################################################################################
 ###################################################################################################
@@ -23,3 +24,25 @@ def test_filter_signal(tsig):
     sigs = np.array([tsig, tsig])
     outs = filter_signal(sigs, FS, 'bandpass', F_RANGE, remove_edges=False)
     assert np.diff(outs, axis=0).sum() == 0
+
+def test_filter_input_checks():
+
+    fir_inputs = {'n_cycles' : 5, 'remove_edges' : False}
+    _filter_input_checks('fir', fir_inputs)
+
+    iir_inputs = {'butterworth_order' : 7}
+    _filter_input_checks('iir', iir_inputs)
+
+    mixed_inputs = {'n_cycles' : 5, 'butterworth_order' : 7}
+    extra_inputs = {'n_cycles' : 5, 'nonsense_input' : True}
+
+    with raises(AssertionError):
+        _filter_input_checks('fir', iir_inputs)
+    with raises(AssertionError):
+        _filter_input_checks('iir', fir_inputs)
+    with raises(AssertionError):
+        _filter_input_checks('fir', mixed_inputs)
+    with raises(AssertionError):
+        _filter_input_checks('fir', mixed_inputs)
+    with raises(AssertionError):
+        _filter_input_checks('fir', extra_inputs)
