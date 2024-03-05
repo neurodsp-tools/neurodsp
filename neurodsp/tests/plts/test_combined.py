cafb078d37efb500d94d08f4f36e14d73b05d82a
Tom Donoghue <tdonoghue.research@gmail.com>
2024-03-05 10:06:07 -0500

    fix tests for name tweak

--- neurodsp/tests/plts/test_combined.py
+++ neurodsp/tests/plts/test_combined.py
@@ -13,20 +13,20 @@ from neurodsp.plts.combined import *
 @plot_test
 def test_plot_timeseries_and_spectrum(tsig, tsig_comb, tsig_burst):
 
-    plot_timeseries_and_spectrum(tsig_comb, FS,
-                                 save_fig=True, file_path=TEST_PLOTS_PATH,
-                                 file_name='test_plot_combined_ts_psd.png')
+    plot_timeseries_and_spectra(tsig_comb, FS,
+                                save_fig=True, file_path=TEST_PLOTS_PATH,
+                                file_name='test_plot_combined_ts_psd.png')
 
     # Test customizations
-    plot_timeseries_and_spectrum(tsig_comb, FS,
-                                 f_range=(3, 50), color='blue',
-                                 spectrum_kwargs={'nperseg' : 500},
-                                 ts_kwargs={'xlim' : [0, 5]},
-                                 psd_kwargs={'lw' : 2},
-                                 save_fig=True, file_path=TEST_PLOTS_PATH,
-                                 file_name='test_plot_combined_ts_psd2.png')
+    plot_timeseries_and_spectra(tsig_comb, FS,
+                                f_range=(3, 50), color='blue',
+                                spectrum_kwargs={'nperseg' : 500},
+                                ts_kwargs={'xlim' : [0, 5]},
+                                psd_kwargs={'lw' : 2},
+                                save_fig=True, file_path=TEST_PLOTS_PATH,
+                                file_name='test_plot_combined_ts_psd2.png')
 
     # Test multi-signal input
-    plot_timeseries_and_spectrum(np.array([tsig, tsig_comb, tsig_burst]), FS,
-                                 save_fig=True, file_path=TEST_PLOTS_PATH,
-                                 file_name='test_plot_combined_ts_psd_multi.png')
+    plot_timeseries_and_spectra(np.array([tsig, tsig_comb, tsig_burst]), FS,
+                                save_fig=True, file_path=TEST_PLOTS_PATH,
+                                file_name='test_plot_combined_ts_psd_multi.png')
