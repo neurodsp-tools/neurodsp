09f137c27656b2f3bc902184b73f766ac3a96b78
Tom Donoghue <tdonoghue.research@gmail.com>
2024-07-19 16:40:10 -0400

    fix up docs & details of sig_sampler

--- neurodsp/sim/multi.py
+++ neurodsp/sim/multi.py
@@ -1,5 +1,7 @@
 """Simulation functions that return multiple instances."""
 
+from collections.abc import Sized
+
 import numpy as np
 
 from neurodsp.utils.core import counter
@@ -43,7 +45,7 @@ def sig_sampler(sim_func, sim_params, return_sim_params=False, n_sims=None):
         Whether to yield the simulation parameters as well as the simulated time series.
     n_sims : int, optional
         Number of simulations to set as the max.
-        If None, creates an infinite generator.
+        If None, length is defined by the length of `sim_params`, and could be infinite.
 
     Yields
     ------
@@ -51,9 +53,12 @@ def sig_sampler(sim_func, sim_params, return_sim_params=False, n_sims=None):
         Simulated time series.
     sample_params : dict
         Simulation parameters for the yielded time series.
+        Only returned if `return_sim_params` is True.
     """
 
-    if len(sim_params) and n_sims and n_sims > len(sim_params):
+    # If `sim_params` has a size, and `n_sims` is defined, check that they are compatible
+    #   To do so, we first check if the iterable has a __len__ attr, and if so check values
+    if isinstance(sim_params, Sized) and len(sim_params) and n_sims and n_sims > len(sim_params):
         msg = 'Cannot simulate the requested number of sims with the given parameters.'
         raise ValueError(msg)
 
