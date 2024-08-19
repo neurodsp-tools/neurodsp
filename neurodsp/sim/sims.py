9406855a7a30d5814655b5c0648eaaa8e13fa659
Tom Donoghue <tdonoghue.research@gmail.com>
2024-08-19 14:07:07 -0400

    add SampledSims object

--- neurodsp/sim/sims.py
+++ neurodsp/sim/sims.py
@@ -70,3 +70,62 @@ class Simulations():
         self.n_seconds = parameters['n_seconds']
         self.fs = parameters['fs']
         self._params = drop_base_params(parameters)
+
+
+class SampledSimulations(Simulations):
+    """Data object for a set of simulated signals with sampled (variable) parameter definitions.
+
+    Parameters
+    ----------
+    signals : 2nd array
+        The simulated signals, organized as [n_sims, sig_length].
+    func : str
+        The simulation function that was used to create the simulations.
+    params : list dict
+        The simulation parameters for each of the simulations.
+
+    Notes
+    -----
+    This object stores a set of simulations with different parameter definitions per signal.
+    """
+
+    def __init__(self, signals=None, func=None, parameters=None):
+        """Initialize VSims object."""
+
+        self._params = []
+        Simulations.__init__(self, signals, func, parameters)
+
+    def add_params(self, parameters):
+        """Add parameter definitions to object."""
+
+        self.n_seconds = parameters[0]['n_seconds']
+        self.fs = parameters[0]['fs']
+
+        self._params = []
+        for cparams in parameters:
+            self._params.append(drop_base_params(cparams))
+
+    @property
+    def params(self):
+        """Define simulation parameters (base + additional parameters) for each simulation."""
+
+        return [{**self._base_params, **self._params[ind]} for ind in range(len(self))]
+
+    def add_signal(self, sig, params=None):
+        """Add a signal to the current object.
+
+        Parameters
+        ----------
+        sig : 1d array
+            A simulated signal to add to the object.
+        params : dict, optional
+            Parameter definition for the added signal.
+            If current object does not include parameters, should be empty.
+            If current object does include parameters, this input is required.
+        """
+
+        self.signals = np.vstack([self.signals, sig])
+        if parameters:
+            self._params.append(drop_base(parameters))
+        else:
+            assert len(self._params) == 0, 'Must add parameters if object already has them.'
--- neurodsp/tests/sim/test_sims.py
+++ neurodsp/tests/sim/test_sims.py
@@ -23,8 +23,33 @@ def test_simulations():
     # Test initialization with data only
     sims_data = Simulations(sigs)
     assert sims_data
+    assert len(sims_data) == n_sigs
 
     # Test initialization with metadata
     sims_full = Simulations(sigs, 'sim_func', params)
     assert len(sims_full) == n_sigs
+    assert sims_full.params == params
+
+def test_sampled_simulations():
+
+    # Test empty initialization
+    sims_empty = SampledSimulations()
+    assert isinstance(sims_empty, SampledSimulations)
+
+    # Demo data
+    n_seconds = 2
+    fs = 100
+    n_sigs = 2
+    sigs = np.zeros([2, n_seconds * fs])
+    params = [{'n_seconds' : n_seconds, 'fs' : fs, 'exponent' : -2},
+              {'n_seconds' : n_seconds, 'fs' : fs, 'exponent' : -1}]
+
+    # Test initialization with data only
+    sims_data = SampledSimulations(sigs)
+    assert sims_data
+    assert len(sims_data) == n_sigs
+
+    # Test initialization with metadata
+    sims_full = SampledSimulations(sigs, 'sim_func', params)
+    assert len(sims_full) == n_sigs == len(sims_full.params)
     assert sims_full.params == params
